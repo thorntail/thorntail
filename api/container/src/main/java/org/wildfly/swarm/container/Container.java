01617d6cb2de94ea48abd87881d2adad1484333a
Bob McWhirter <bob@mcwhirter.org>
2015-06-08 12:23:13 -0400

    Add a .java license header for copy/paste easiness.

--- api/container/src/main/java/org/wildfly/swarm/container/Container.java
+++ api/container/src/main/java/org/wildfly/swarm/container/Container.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package org.wildfly.swarm.container;
 
 import org.jboss.modules.Module;
