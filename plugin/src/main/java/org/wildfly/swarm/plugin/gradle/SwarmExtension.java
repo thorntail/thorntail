8da367347051ab997b7e69bf0324c8fa32dccca5
Amann Malik <amannmalik@gmail.com>
2015-07-09 11:35:01 -0500

    enable additional configuration parameters missing from Gradle plugin

--- plugin/src/main/java/org/wildfly/swarm/plugin/gradle/SwarmExtension.java
+++ plugin/src/main/java/org/wildfly/swarm/plugin/gradle/SwarmExtension.java
@@ -5,6 +5,11 @@ package org.wildfly.swarm.plugin.gradle;
  */
 public class SwarmExtension {
     private String mainClass;
+    private Integer httpPort;
+    private Integer portOffset;
+    private String bindAddress;
+    private String contextPath;
+    private Boolean bundleDependencies;
 
     public SwarmExtension() {
 
@@ -16,5 +21,45 @@ public class SwarmExtension {
 
     public String getMainClassName() {
         return this.mainClass;
+    }
+
+    public Integer getHttpPort() {
+        return httpPort;
+    }
+
+    public void setHttpPort(Integer httpPort) {
+        this.httpPort = httpPort;
+    }
+
+    public Integer getPortOffset() {
+        return portOffset;
+    }
+
+    public void setPortOffset(Integer portOffset) {
+        this.portOffset = portOffset;
+    }
+
+    public String getBindAddress() {
+        return bindAddress;
+    }
+
+    public void setBindAddress(String bindAddress) {
+        this.bindAddress = bindAddress;
+    }
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public Boolean getBundleDependencies() {
+        return bundleDependencies;
+    }
+
+    public void setBundleDependencies(Boolean bundleDependencies) {
+        this.bundleDependencies = bundleDependencies;
     }
 }
