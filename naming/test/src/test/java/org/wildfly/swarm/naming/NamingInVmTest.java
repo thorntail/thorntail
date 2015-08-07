2b7b45ca4545945a94a936a6d50b44e06e4e69d3
Bob McWhirter <bob@mcwhirter.org>
2015-08-07 16:56:29 -0400

    Add some basic tests for Naming fraction.

---
+++ naming/test/src/test/java/org/wildfly/swarm/naming/NamingInVmTest.java
@@ -0,0 +1,17 @@
+package org.wildfly.swarm.naming;
+
+import org.junit.Test;
+import org.wildfly.swarm.container.Container;
+
+/**
+ * @author Bob McWhirter
+ */
+public class NamingInVmTest {
+
+    @Test
+    public void testSimple() throws Exception {
+        Container container = new Container();
+        container.fraction( new NamingFraction() );
+        container.start().stop();
+    }
+}
