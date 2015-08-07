56265d44232f2c2da1250dd795baad1842742f4c
Bob McWhirter <bob@mcwhirter.org>
2015-08-07 16:16:10 -0400

    Migrate bean-validation

---
+++ bean-validation/api/src/main/java/org/wildfly/swarm/bean/validation/BeanValidationRuntimeModuleProvider.java
@@ -0,0 +1,18 @@
+package org.wildfly.swarm.bean.validation;
+
+import org.wildfly.swarm.container.RuntimeModuleProvider;
+
+/**
+ * @author Bob McWhirter
+ */
+public class BeanValidationRuntimeModuleProvider implements RuntimeModuleProvider {
+    @Override
+    public String getModuleName() {
+        return "org.wildfly.swarm.bean-validation";
+    }
+
+    @Override
+    public String getSlotName() {
+        return "runtime";
+    }
+}
---
+++ bean-validation/runtime/src/main/java/org/wildfly/swarm/bean/validation/runtime/BeanValidationConfiguration.java
@@ -0,0 +1,51 @@
+package org.wildfly.swarm.bean.validation.runtime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.dmr.ModelNode;
+import org.wildfly.swarm.bean.validation.BeanValidationFraction;
+import org.wildfly.swarm.container.runtime.AbstractServerConfiguration;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXTENSION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+
+/**
+ * @author Bob McWhirter
+ */
+public class BeanValidationConfiguration extends AbstractServerConfiguration<BeanValidationFraction> {
+
+    public BeanValidationConfiguration() {
+        super(BeanValidationFraction.class);
+    }
+
+    @Override
+    public BeanValidationFraction defaultFraction() {
+        return new BeanValidationFraction();
+    }
+
+    @Override
+    public List<ModelNode> getList(BeanValidationFraction fraction) {
+
+        List<ModelNode> list = new ArrayList<>();
+        PathAddress address = PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, "bean-validation"));
+
+        ModelNode node = new ModelNode();
+        node.get(OP_ADDR).set(EXTENSION, "org.wildfly.extension.bean-validation");
+        node.get(OP).set(ADD);
+        list.add(node);
+
+        node = new ModelNode();
+        node.get(OP_ADDR).set(address.toModelNode());
+        node.get(OP).set(ADD);
+        list.add(node);
+
+        return list;
+
+    }
+}
