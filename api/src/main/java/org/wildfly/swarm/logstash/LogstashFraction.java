346b2abbd48a0b7d12c6f3bd483713121317f921
Bob McWhirter <bob@mcwhirter.org>
2015-08-10 16:15:59 -0400

    Migrate ribbon and dependencies.

---
+++ api/src/main/java/org/wildfly/swarm/logstash/LogstashFraction.java
@@ -0,0 +1,60 @@
+package org.wildfly.swarm.logstash;
+
+import java.util.Properties;
+
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.container.Fraction;
+import org.wildfly.swarm.logging.LoggingFraction;
+
+/**
+ * @author Ken Finnigan
+ */
+public class LogstashFraction implements Fraction {
+
+    private Properties handlerProperties = new Properties();
+    private Properties formatterProperties = new Properties();
+
+    private String level = "INFO";
+
+    public LogstashFraction() {
+        this("wildflySwarmNode", "${jboss.node.name}");
+        hostname( "${logstash.host}" );
+    }
+
+    public LogstashFraction(String nodeKey, String nodeValue) {
+        this.formatterProperties.put(nodeKey, nodeValue);
+    }
+
+    public LogstashFraction level(String level) {
+        this.level = level;
+        return this;
+    }
+
+    public LogstashFraction hostname(String hostname) {
+        this.handlerProperties.put("hostname", "${swarm.logstash.hostname:" + hostname + "}");
+        return this;
+    }
+
+    public LogstashFraction port(String port) {
+        this.handlerProperties.put("port", "${swarm.logstash.port:" + port + "}");
+        return this;
+    }
+
+    public LogstashFraction port(int port) {
+        port( "" + port );
+        return this;
+    }
+
+    public LogstashFraction metadata(String key, String value) {
+        this.formatterProperties.put(key, value);
+        return this;
+    }
+
+    @Override
+    public void initialize(Container.InitContext initContext) {
+        initContext.fraction(new LoggingFraction()
+                .customFormatter("logstash", "org.jboss.logmanager.ext", "org.jboss.logmanager.ext.formatters.LogstashFormatter", this.formatterProperties)
+                .customHandler("logstash-handler", "org.jboss.logmanager.ext", "org.jboss.logmanager.ext.handlers.SocketHandler", this.handlerProperties, "logstash")
+                .rootLogger(this.level));
+    }
+}
