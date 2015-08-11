91fc007de3dd810bdb2a727b3b702ad8098ff0f0
Bob McWhirter <bob@mcwhirter.org>
2015-08-11 15:57:32 -0400

    Rewire dependencies between jpa->datasources->jca->connector.

--- jca/api/src/main/java/org/wildfly/swarm/jca/JCARuntimeModuleProvider.java
+++ jca/api/src/main/java/org/wildfly/swarm/jca/JCARuntimeModuleProvider.java
@@ -8,6 +8,11 @@ import org.wildfly.swarm.container.RuntimeModuleProvider;
 public class JCARuntimeModuleProvider implements RuntimeModuleProvider {
     @Override
     public String getModuleName() {
-        return "org.wildfly.swarm.runtime.jca";
+        return "org.wildfly.swarm.jca";
+    }
+
+    @Override
+    public String getSlotName() {
+        return "runtime";
     }
 }
