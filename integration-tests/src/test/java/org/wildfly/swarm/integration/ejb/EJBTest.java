b68937fc2a380f41ec585c445559e91d1c056cc9
Ken Finnigan <ken@kenfinnigan.me>
2015-08-12 22:01:09 -0400

    Enable EJB test, except for in container

--- integration-tests/src/test/java/org/wildfly/swarm/integration/ejb/EJBTest.java
+++ integration-tests/src/test/java/org/wildfly/swarm/integration/ejb/EJBTest.java
@@ -1,19 +1,24 @@
 package org.wildfly.swarm.integration.ejb;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.ejb.EJB;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.swarm.integration.base.AbstractWildFlySwarmTestCase;
+import org.openqa.selenium.WebDriver;
+import org.wildfly.swarm.arquillian.adapter.ArtifactDependencies;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -21,29 +26,46 @@ import static org.fest.assertions.Assertions.assertThat;
 /**
  * @author Ken Finnigan
  */
-//@RunWith(Arquillian.class)
-public class EJBTest extends AbstractWildFlySwarmTestCase {
+@RunWith(Arquillian.class)
+public class EJBTest {
 
-    @Deployment
-    public static Archive createDeployment() throws Exception {
-        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class, "test.war");
-        deployment.addResource(MyResource.class);
-        deployment.addClass(GreeterEJB.class);
-        deployment.addAllDependencies();
-        return deployment;
-    }
+    @ArquillianResource
+    URL contextRoot;
 
-//    @Test @RunAsClient
-    public void testFromOutside() throws IOException {
-        assertThat(fetch("http://localhost:8080/")).contains("Howdy from EJB");
-    }
+    @Drone
+    WebDriver browser;
 
     @EJB
     private GreeterEJB greeter;
 
-//    @Test
+    @Deployment
+    public static Archive createDeployment() throws Exception {
+        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class, "ejb-test.war");
+        deployment.addResource(MyResource.class);
+        deployment.addClass(GreeterEJB.class);
+        return deployment;
+    }
+
+    @ArtifactDependencies
+    public static List<String> artifactDependencies() {
+        return Arrays.asList(
+                "org.wildfly.swarm:wildfly-swarm-ejb",
+                "org.wildfly.swarm:wildfly-swarm-jaxrs"
+        );
+    }
+
+    @Test
+    @RunAsClient
+    public void testFromOutside() throws IOException {
+        browser.navigate().to(contextRoot);
+
+        assertThat(browser.getPageSource()).contains("Howdy from EJB");
+    }
+
+    @Ignore
+    @Test
     public void testFromInside() {
-        assertThat( greeter.message() ).isEqualTo( "Howdy from EJB" );
+        assertThat(greeter.message()).isEqualTo("Howdy from EJB");
     }
 
 }
