1b82b3f9725970a4329d014a79efc0663ae4c0e2
Lance Ball <lball@redhat.com>
2015-09-10 11:19:09 -0400

    Be sure to use named formatters in logging config

--- logging/api/src/main/java/org/wildfly/swarm/logging/LoggingFraction.java
+++ logging/api/src/main/java/org/wildfly/swarm/logging/LoggingFraction.java
@@ -187,7 +187,7 @@ public class LoggingFraction implements Fraction {
         this.logging.consoleHandler(
                 new ConsoleHandler(CONSOLE)
                         .level(level)
-                        .formatter(formatter)
+                        .namedFormatter(formatter)
         );
         return this;
     }
