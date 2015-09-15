fd54e8cadba8195bd3de82e08fc1dafc808ad081
Lance Ball <lball@redhat.com>
2015-09-15 14:00:57 -0400

    Integrate bean-validation config API

--- bean-validation/runtime/src/main/java/org/wildfly/swarm/bean/validation/runtime/BeanValidationConfiguration.java
+++ bean-validation/runtime/src/main/java/org/wildfly/swarm/bean/validation/runtime/BeanValidationConfiguration.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.PathElement;
 import org.jboss.dmr.ModelNode;
+import org.wildfly.apigen.invocation.Marshaller;
 import org.wildfly.swarm.bean.validation.BeanValidationFraction;
+import org.wildfly.swarm.config.bean.validation.BeanValidation;
 import org.wildfly.swarm.container.runtime.AbstractServerConfiguration;
 
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
@@ -40,10 +42,12 @@ public class BeanValidationConfiguration extends AbstractServerConfiguration<Bea
         node.get(OP).set(ADD);
         list.add(node);
 
-        node = new ModelNode();
-        node.get(OP_ADDR).set(address.toModelNode());
-        node.get(OP).set(ADD);
-        list.add(node);
+        BeanValidation beanValidation = new BeanValidation();
+        try {
+            list.addAll(Marshaller.marshal(beanValidation));
+        } catch (Exception e) {
+            System.err.println("Unable to configure bean-validation " + e);
+        }
 
         return list;
 
