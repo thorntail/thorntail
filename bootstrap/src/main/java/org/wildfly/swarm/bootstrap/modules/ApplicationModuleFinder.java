c732475b466909f84f35fd6375379f984bc9c37c
Bob McWhirter <bob@mcwhirter.org>
2015-10-12 15:52:11 -0400

    Add sun.jdk:main to swarm.application:main, to support jruby's weirdness.

--- bootstrap/src/main/java/org/wildfly/swarm/bootstrap/modules/ApplicationModuleFinder.java
+++ bootstrap/src/main/java/org/wildfly/swarm/bootstrap/modules/ApplicationModuleFinder.java
@@ -79,6 +79,7 @@ public class ApplicationModuleFinder implements ModuleFinder {
         builder.addDependency(DependencySpec.createModuleDependencySpec(ModuleIdentifier.create("org.jboss.msc")));
         builder.addDependency(DependencySpec.createModuleDependencySpec(ModuleIdentifier.create("org.jboss.shrinkwrap")));
         builder.addDependency(DependencySpec.createModuleDependencySpec(ModuleIdentifier.create("javax.api")));
+        builder.addDependency(DependencySpec.createModuleDependencySpec(ModuleIdentifier.create("sun.jdk")));
 
         builder.addDependency(
                 DependencySpec.createModuleDependencySpec(
