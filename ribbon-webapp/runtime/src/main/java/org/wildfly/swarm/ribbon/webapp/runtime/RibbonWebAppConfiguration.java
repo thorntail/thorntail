9ce4fe02b130d01ed81c1022bcf946834cba92e6
Bob McWhirter <bob@mcwhirter.org>
2015-10-30 11:32:30 -0400

    * Use the runtime classloader to find ribbon.js * Shuffle some dependencies in api to ensure ribbon/clustering/jmx is included * cast the archive to a RibbonArchive so it participates in the topology.

--- ribbon-webapp/runtime/src/main/java/org/wildfly/swarm/ribbon/webapp/runtime/RibbonWebAppConfiguration.java
+++ ribbon-webapp/runtime/src/main/java/org/wildfly/swarm/ribbon/webapp/runtime/RibbonWebAppConfiguration.java
@@ -1,9 +1,11 @@
 package org.wildfly.swarm.ribbon.webapp.runtime;
 
+import com.netflix.ribbon.Ribbon;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.container.runtime.AbstractServerConfiguration;
+import org.wildfly.swarm.netflix.ribbon.RibbonArchive;
 import org.wildfly.swarm.ribbon.webapp.RibbonWebAppFraction;
 import org.wildfly.swarm.undertow.WARArchive;
 
@@ -30,8 +32,9 @@ public class RibbonWebAppConfiguration extends AbstractServerConfiguration<Ribbo
         List<Archive> list = new ArrayList<>();
         WARArchive war = ShrinkWrap.create( WARArchive.class );
         war.addClass( RibbonToTheCurbSSEServlet.class );
-        war.addModule("org.wildfly.swarm.ribbon");
-        war.addAsResource(new ClassLoaderAsset("ribbon.js"), "js/ribbon.js");
+        war.addModule("org.wildfly.swarm.netflix.ribbon");
+        war.addAsResource(new ClassLoaderAsset("ribbon.js", this.getClass().getClassLoader()), "js/ribbon.js");
+        war.as(RibbonArchive.class);
         list.add(war);
         return list;
     }
