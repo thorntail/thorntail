fb8fe7b4fd7098038272c4708d583dad948e53ef
Toby Crawley <toby@tcrawley.org>
2015-12-04 12:14:03 -0500

    Add auxiliary jars as libs to wars [SWARM-138]
    For wars, we can't merge the aux libs at the top-level, they need to be
    in WEB-INF/lib for the deployment's classloader to find them.

--- arquillian/daemon/api/src/main/java/org/wildfly/swarm/arquillian/daemon/protocol/DaemonDeploymentPackager.java
+++ arquillian/daemon/api/src/main/java/org/wildfly/swarm/arquillian/daemon/protocol/DaemonDeploymentPackager.java
@@ -28,6 +28,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import org.jboss.arquillian.container.spi.client.deployment.Validate;
 import org.jboss.arquillian.container.test.spi.TestDeployment;
 import org.jboss.arquillian.container.test.spi.client.deployment.DeploymentPackager;
 import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
@@ -36,7 +37,10 @@ import org.jboss.shrinkwrap.api.ArchivePath;
 import org.jboss.shrinkwrap.api.Filter;
 import org.jboss.shrinkwrap.api.Node;
 import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.container.LibraryContainer;
+import org.jboss.shrinkwrap.api.container.WebContainer;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.impl.base.asset.ServiceProviderAsset;
 
 /**
@@ -55,10 +59,24 @@ public enum DaemonDeploymentPackager implements DeploymentPackager {
     public Archive<?> generateDeployment(final TestDeployment testDeployment,
         final Collection<ProtocolArchiveProcessor> processors) {
         // Merge auxiliary archives with the declared for ARQ and testrunner support
-        final JavaArchive archive = testDeployment.getApplicationArchive().as(JavaArchive.class);
+        final Archive archive = testDeployment.getApplicationArchive();
         if (log.isLoggable(Level.FINEST)) {
             log.finest("Archive before additional packaging: " + archive.toString(true));
         }
+        if (Validate.isArchiveOfType(WebArchive.class, archive)) {
+            ((LibraryContainer<?>)archive).addAsLibraries(testDeployment.getAuxiliaryArchives());
+        } else {
+            mergeAuxAsClasses(archive, testDeployment);
+        }
+
+        if (log.isLoggable(Level.FINEST)) {
+            log.finest("Archive after additional packaging: " + archive.toString(true));
+        }
+
+        return archive;
+    }
+
+    private void mergeAuxAsClasses(final Archive<?> archive, final TestDeployment testDeployment) {
         final Map<ArchivePath, List<Node>> serviceDescriptors = new HashMap<>();
         findServiceDescriptors(archive, serviceDescriptors);
         testDeployment.getAuxiliaryArchives().forEach(aux -> {
@@ -69,21 +87,16 @@ public enum DaemonDeploymentPackager implements DeploymentPackager {
         mergeServiceDescriptors(serviceDescriptors)
                 .forEach((path, asset) -> archive.add(asset, path));
 
-        if (log.isLoggable(Level.FINEST)) {
-            log.finest("Archive after additional packaging: " + archive.toString(true));
-        }
-
-        return archive;
     }
 
-    private Map<ArchivePath, Asset> mergeServiceDescriptors(Map<ArchivePath, List<Node>> descs) {
+    private Map<ArchivePath, Asset> mergeServiceDescriptors(final Map<ArchivePath, List<Node>> descs) {
         return descs.entrySet()
                 .stream()
                 .collect(Collectors.toMap(Map.Entry::getKey,
                                           entry -> mergeServiceDescriptors(entry.getValue())));
     }
 
-    private Asset mergeServiceDescriptors(List<Node> descs) {
+    private Asset mergeServiceDescriptors(final List<Node> descs) {
         final List<String> lines = new ArrayList<>();
         descs.forEach(n -> {
             try (BufferedReader in = new BufferedReader(new InputStreamReader(n.getAsset().openStream())) ) {
@@ -99,7 +112,7 @@ public enum DaemonDeploymentPackager implements DeploymentPackager {
         return new ServiceProviderAsset(lines.toArray(new String[lines.size()]));
     }
 
-    private void findServiceDescriptors(Archive<?> archive, Map<ArchivePath, List<Node>> descs) {
+    private void findServiceDescriptors(final Archive<?> archive, final Map<ArchivePath, List<Node>> descs) {
         archive.getContent(path -> path.get().startsWith("/META-INF/services/"))
                 .forEach((path, node) -> {
                     List<Node> nodes = descs.get(path);
