9eea7f07ee83901fa1478b05290e11ebb34e547b
Toby Crawley <toby@tcrawley.org>
2015-12-03 23:13:22 -0500

    Support test enrichers again.
    We have to merge service descriptors when building the archive,
    otherwise the last writer wins.
    
    ContainerTestRemoteExtension (which is responsible for actually running
    the tests in-container) is loaded via
    META-INF/services/org.jboss.arquillian.container.test.spi.RemoteLoadableExtension,
    as are all test enrichers.

--- arquillian/daemon/api/src/main/java/org/wildfly/swarm/arquillian/daemon/protocol/DaemonDeploymentPackager.java
+++ arquillian/daemon/api/src/main/java/org/wildfly/swarm/arquillian/daemon/protocol/DaemonDeploymentPackager.java
@@ -16,20 +16,34 @@
  */
 package org.wildfly.swarm.arquillian.daemon.protocol;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.jboss.arquillian.container.test.spi.TestDeployment;
 import org.jboss.arquillian.container.test.spi.client.deployment.DeploymentPackager;
 import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePath;
+import org.jboss.shrinkwrap.api.Filter;
+import org.jboss.shrinkwrap.api.Node;
+import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.impl.base.asset.ServiceProviderAsset;
 
 /**
  * {@link DeploymentPackager} to merge auxiliar archive contents with the archive provided by the user
  *
  * @author <a href="mailto:alr@jboss.org">Andrew Lee Rubinger</a>
+ * @author Toby Crawley
  */
 public enum DaemonDeploymentPackager implements DeploymentPackager {
 
@@ -37,12 +51,6 @@ public enum DaemonDeploymentPackager implements DeploymentPackager {
 
     private static final Logger log = Logger.getLogger(DaemonDeploymentPackager.class.getName());
 
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.jboss.arquillian.container.test.spi.client.deployment.DeploymentPackager#generateDeployment(org.jboss.arquillian
-     *      .container.test.spi.TestDeployment, java.util.Collection)
-     */
     @Override
     public Archive<?> generateDeployment(final TestDeployment testDeployment,
         final Collection<ProtocolArchiveProcessor> processors) {
@@ -51,14 +59,55 @@ public enum DaemonDeploymentPackager implements DeploymentPackager {
         if (log.isLoggable(Level.FINEST)) {
             log.finest("Archive before additional packaging: " + archive.toString(true));
         }
-        for (final Archive<?> auxArchive : testDeployment.getAuxiliaryArchives()) {
-            archive.merge(auxArchive);
-        }
+        final Map<ArchivePath, List<Node>> serviceDescriptors = new HashMap<>();
+        findServiceDescriptors(archive, serviceDescriptors);
+        testDeployment.getAuxiliaryArchives().forEach(aux -> {
+            findServiceDescriptors(aux, serviceDescriptors);
+            archive.merge(aux);
+        });
+
+        mergeServiceDescriptors(serviceDescriptors)
+                .forEach((path, asset) -> archive.add(asset, path));
+
         if (log.isLoggable(Level.FINEST)) {
             log.finest("Archive after additional packaging: " + archive.toString(true));
         }
 
         return archive;
+    }
 
+    private Map<ArchivePath, Asset> mergeServiceDescriptors(Map<ArchivePath, List<Node>> descs) {
+        return descs.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                                          entry -> mergeServiceDescriptors(entry.getValue())));
+    }
+
+    private Asset mergeServiceDescriptors(List<Node> descs) {
+        final List<String> lines = new ArrayList<>();
+        descs.forEach(n -> {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(n.getAsset().openStream())) ) {
+                String line;
+                while ((line = in.readLine()) != null) {
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return new ServiceProviderAsset(lines.toArray(new String[lines.size()]));
+    }
+
+    private void findServiceDescriptors(Archive<?> archive, Map<ArchivePath, List<Node>> descs) {
+        archive.getContent(path -> path.get().startsWith("/META-INF/services/"))
+                .forEach((path, node) -> {
+                    List<Node> nodes = descs.get(path);
+                    if (nodes == null) {
+                        nodes = new ArrayList<>();
+                        descs.put(path, nodes);
+                    }
+                    nodes.add(node);
+                });
     }
 }
