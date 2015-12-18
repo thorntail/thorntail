329120194a2c5a603158973d8430c04216532899
Toby Crawley <toby@tcrawley.org>
2015-12-18 12:57:34 -0500

    Only create subsystem-specific cache containers if the fractions are available [SWARM-198]

--- infinispan/api/src/main/java/org/wildfly/swarm/infinispan/InfinispanFraction.java
+++ infinispan/api/src/main/java/org/wildfly/swarm/infinispan/InfinispanFraction.java
@@ -46,9 +46,9 @@ public class InfinispanFraction extends Infinispan<InfinispanFraction> implement
     public void postInitialize(Container.PostInitContext initContext) {
         if (this.defaultFraction) {
             if (initContext.hasFraction("jgroups")) {
-                clusteredDefaultFraction();
+                clusteredDefaultFraction(initContext);
             } else {
-                localDefaultFraction();
+                localDefaultFraction(initContext);
             }
         }
     }
@@ -68,7 +68,7 @@ public class InfinispanFraction extends Infinispan<InfinispanFraction> implement
         return this;
     }
 
-    private InfinispanFraction clusteredDefaultFraction() {
+    private InfinispanFraction clusteredDefaultFraction(Container.PostInitContext initContext) {
         cacheContainer("server",
                        cc -> cc.defaultCache("default")
                                .alias("singleton")
@@ -77,90 +77,102 @@ public class InfinispanFraction extends Infinispan<InfinispanFraction> implement
                                .replicatedCache("default",
                                                 c -> c.mode(Mode.SYNC)
                                                         .transactionComponent(t -> t.mode(TransactionComponent.Mode.BATCH))));
+        if (initContext.hasFraction("undertow")) {
+            cacheContainer("web",
+                           cc -> cc.defaultCache("dist")
+                                   .jgroupsTransport(t -> t.lockTimeout(60000L))
+                                   .distributedCache("dist",
+                                                     c -> c.mode(Mode.ASYNC)
+                                                             .l1Lifespan(0L)
+                                                             .owners(2)
+                                                             .lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
+                                                             .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
+                                                             .fileStore()));
+        }
 
-        cacheContainer("web",
-                       cc -> cc.defaultCache("dist")
-                               .jgroupsTransport(t -> t.lockTimeout(60000L))
-                               .distributedCache("dist",
-                                                 c -> c.mode(Mode.ASYNC)
-                                                         .l1Lifespan(0L)
-                                                         .owners(2)
-                                                         .lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
-                                                         .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
-                                                         .fileStore()));
+        if (initContext.hasFraction("ejb")) {
+            cacheContainer("ejb",
+                           cc -> cc.defaultCache("dist")
+                                   .alias("sfsb")
+                                   .jgroupsTransport(t -> t.lockTimeout(60000L))
+                                   .distributedCache("dist",
+                                                     c -> c.mode(Mode.ASYNC)
+                                                             .l1Lifespan(0L)
+                                                             .owners(2)
+                                                             .lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
+                                                             .transactionComponent(t -> t.mode(TransactionComponent.Mode.BATCH))
+                                                             .fileStore()));
 
-        cacheContainer("ejb",
-                       cc -> cc.defaultCache("dist")
-                               .alias("sfsb")
-                               .jgroupsTransport(t -> t.lockTimeout(60000L))
-                               .distributedCache("dist",
-                                                 c -> c.mode(Mode.ASYNC)
-                                                         .l1Lifespan(0L)
-                                                         .owners(2)
-                                                         .lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
-                                                         .transactionComponent(t -> t.mode(TransactionComponent.Mode.BATCH))
-                                                         .fileStore()));
+        }
 
-        cacheContainer("hibernate",
-                       cc -> cc.defaultCache("local-query")
-                               .jgroupsTransport(t -> t.lockTimeout(60000L))
-                               .localCache("local-query",
-                                           c -> c.evictionComponent(ec -> ec.maxEntries(10000L).strategy(EvictionComponent.Strategy.LRU))
-                                                   .expirationComponent(ec -> ec.maxIdle(100000L)))
-                               .invalidationCache("entity",
-                                                  c -> c.mode(Mode.SYNC)
-                                                          .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.NON_XA))
-                                                          .evictionComponent(ec -> ec.maxEntries(10000L).strategy(EvictionComponent.Strategy.LRU))
-                                                          .expirationComponent(ec -> ec.maxIdle(100000L)))
-                               .replicatedCache("timestamps", c -> c.mode(Mode.ASYNC)));
+        if (initContext.hasFraction("jpa")) {
+            cacheContainer("hibernate",
+                           cc -> cc.defaultCache("local-query")
+                                   .jgroupsTransport(t -> t.lockTimeout(60000L))
+                                   .localCache("local-query",
+                                               c -> c.evictionComponent(ec -> ec.maxEntries(10000L).strategy(EvictionComponent.Strategy.LRU))
+                                                       .expirationComponent(ec -> ec.maxIdle(100000L)))
+                                   .invalidationCache("entity",
+                                                      c -> c.mode(Mode.SYNC)
+                                                              .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.NON_XA))
+                                                              .evictionComponent(ec -> ec.maxEntries(10000L).strategy(EvictionComponent.Strategy.LRU))
+                                                              .expirationComponent(ec -> ec.maxIdle(100000L)))
+                                   .replicatedCache("timestamps", c -> c.mode(Mode.ASYNC)));
+        }
 
         return this;
     }
 
-    private InfinispanFraction localDefaultFraction() {
+    private InfinispanFraction localDefaultFraction(Container.PostInitContext initContext) {
         cacheContainer("server",
                        cc -> cc.defaultCache("default")
                                .localCache("default", c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.BATCH)))
                                .remoteCommandThreadPool());
 
-        cacheContainer("web",
-                       cc -> cc.defaultCache("passivation")
-                               .localCache("passivation",
-                                           c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
-                                                   .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
-                                                   .fileStore(fs -> fs.passivation(true).purge(false)))
-                               .localCache("persistent",
-                                           c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
-                                                   .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
-                                                   .fileStore(fs -> fs.passivation(false).purge(false))));
+        if (initContext.hasFraction("undertow")) {
+            cacheContainer("web",
+                           cc -> cc.defaultCache("passivation")
+                                   .localCache("passivation",
+                                               c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
+                                                       .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
+                                                       .fileStore(fs -> fs.passivation(true).purge(false)))
+                                   .localCache("persistent",
+                                               c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
+                                                       .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
+                                                       .fileStore(fs -> fs.passivation(false).purge(false))));
+        }
 
-        cacheContainer("ejb",
-                       cc -> cc.alias("sfsb")
-                               .defaultCache("passivation")
-                               .localCache("passivation",
-                                           c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
-                                                   .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
-                                                   .fileStore(fs -> fs.passivation(true).purge(false)))
-                               .localCache("persistent",
-                                           c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
-                                                   .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
-                                                   .fileStore(fs -> fs.passivation(false).purge(false))));
+        if (initContext.hasFraction("ejb")) {
+            cacheContainer("ejb",
+                           cc -> cc.alias("sfsb")
+                                   .defaultCache("passivation")
+                                   .localCache("passivation",
+                                               c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
+                                                       .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
+                                                       .fileStore(fs -> fs.passivation(true).purge(false)))
+                                   .localCache("persistent",
+                                               c -> c.lockingComponent(lc -> lc.isolation(LockingComponent.Isolation.REPEATABLE_READ))
+                                                       .transactionComponent(tc -> tc.mode(TransactionComponent.Mode.BATCH))
+                                                       .fileStore(fs -> fs.passivation(false).purge(false))));
+        }
 
-        cacheContainer("hibernate",
-                       cc -> cc.defaultCache("local-query")
-                               .localCache("entity",
-                                           c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.NON_XA))
-                                                   .evictionComponent(e -> e.strategy(EvictionComponent.Strategy.LRU).maxEntries(10000L))
-                                                   .expirationComponent(e -> e.maxIdle(100000L)))
-                               .localCache("immutable-entity",
-                                           c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.NON_XA))
-                                                   .evictionComponent(e -> e.strategy(EvictionComponent.Strategy.LRU).maxEntries(10000L))
-                                                   .expirationComponent(e -> e.maxIdle(100000L)))
-                               .localCache("local-query",
-                                           c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.NON_XA))
-                                                   .evictionComponent(e -> e.strategy(EvictionComponent.Strategy.LRU).maxEntries(10000L))
-                                                   .expirationComponent(e -> e.maxIdle(100000L)))
-                               .localCache("timestamps"));
+        if (initContext.hasFraction("jpa")) {
+            cacheContainer("hibernate",
+                           cc -> cc.defaultCache("local-query")
+                                   .localCache("entity",
+                                               c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.NON_XA))
+                                                       .evictionComponent(e -> e.strategy(EvictionComponent.Strategy.LRU).maxEntries(10000L))
+                                                       .expirationComponent(e -> e.maxIdle(100000L)))
+                                   .localCache("immutable-entity",
+                                               c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.NON_XA))
+                                                       .evictionComponent(e -> e.strategy(EvictionComponent.Strategy.LRU).maxEntries(10000L))
+                                                       .expirationComponent(e -> e.maxIdle(100000L)))
+                                   .localCache("local-query",
+                                               c -> c.transactionComponent(t -> t.mode(TransactionComponent.Mode.NON_XA))
+                                                       .evictionComponent(e -> e.strategy(EvictionComponent.Strategy.LRU).maxEntries(10000L))
+                                                       .expirationComponent(e -> e.maxIdle(100000L)))
+                                   .localCache("timestamps"));
+        }
 
         return this;
     }
