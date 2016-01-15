a9eec247f064bfce52081c92caf9d074b369bc87
Ben Browning <bbrownin@redhat.com>
2016-01-15 13:48:59 -0500

    Make sure ribbon-webapp can see the swarm application's classes

--- ribbon-webapp/runtime/src/main/java/org/wildfly/swarm/ribbon/webapp/runtime/RibbonWebAppConfiguration.java
+++ ribbon-webapp/runtime/src/main/java/org/wildfly/swarm/ribbon/webapp/runtime/RibbonWebAppConfiguration.java
@@ -54,7 +54,7 @@ public class RibbonWebAppConfiguration extends AbstractServerConfiguration<Ribbo
         WARArchive war = ShrinkWrap.create( WARArchive.class, "ribbon-webapp.war" );
         war.addAsWebInfResource(new StringAsset(getWebXml(fraction)), "web.xml");
         war.addClass( RibbonToTheCurbSSEServlet.class );
-        war.addClass(fraction.externalAddressMapper());
+        war.addModule("swarm.application");
         war.addModule("org.wildfly.swarm.netflix.ribbon");
         war.addAsWebResource(new ClassLoaderAsset("ribbon.js", this.getClass().getClassLoader()), "ribbon.js");
         war.setContextRoot(context);
