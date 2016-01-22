f284359c3e8fd02fc7378fe02da135c8a324a2a8
Bob McWhirter <bob@mcwhirter.org>
2016-01-22 14:09:49 -0500

    Break apart Ribbon from Topology-management.
    Ribbon normally uses Eureka, but initially we replaced it with
    JGroups, for service discovery.  This ended up being sub-par, as
    there are other service discovery mechanisms (such as Eureka, or
    Consul) which users may want to use.
    
    By dividing the Ribbon and Topology bits, all of our Ribbon stuff
    is topology-infrastructure agnostic, and now has a Topology abstraction
    between itself and JGroups or other mechanisms.
    
    This has introduced a 'topology' fraction which Ribbon talks to
    on one side, and begins specific implementations (such as topology-jgroups)
    to populate information from the other side.
    
    Another benefit of this is that topology facilities may be used
    without involving Ribbon at all now.
    
    Additionaly, move ribbon-webapp to topology-webapp, along with
    renaming the bits to /topology/topology.js and /topology/system/stream.

---
+++ runtime/src/main/java/org/wildfly/swarm/topology/jgroups/runtime/JGroupsTopologyConnectorActivator.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.topology.jgroups.runtime;
+
+import org.jboss.as.network.SocketBinding;
+import org.jboss.msc.service.ServiceActivator;
+import org.jboss.msc.service.ServiceActivatorContext;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceRegistryException;
+import org.jboss.msc.service.ServiceTarget;
+import org.wildfly.clustering.dispatcher.CommandDispatcherFactory;
+import org.wildfly.swarm.topology.runtime.TopologyConnector;
+import org.wildfly.swarm.topology.runtime.TopologyManager;
+
+/**
+ * @author Bob McWhirter
+ */
+public class JGroupsTopologyConnectorActivator implements ServiceActivator {
+
+    @Override
+    public void activate(ServiceActivatorContext context) throws ServiceRegistryException {
+        ServiceTarget target = context.getServiceTarget();
+
+        JGroupsTopologyConnector manager = new JGroupsTopologyConnector();
+
+        target.addService(TopologyConnector.SERVICE_NAME, manager)
+                .addDependency(ServiceName.parse("jboss.clustering.dispatcher.default"), CommandDispatcherFactory.class, manager.getCommandDispatcherFactoryInjector())
+                .addDependency(ServiceName.parse("org.wildfly.network.socket-binding.http"), SocketBinding.class, manager.getSocketBindingInjector())
+                .addDependency(TopologyManager.SERVICE_NAME, TopologyManager.class, manager.getTopologyManagerInjector())
+                .install();
+
+    }
+}
