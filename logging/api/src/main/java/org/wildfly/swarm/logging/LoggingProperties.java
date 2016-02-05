f5c87b50e73b88b20393a5bd7cf90edf85a46dea
Bob McWhirter <bob@mcwhirter.org>
2016-02-05 16:47:43 -0500

    Add some license headers.

--- logging/api/src/main/java/org/wildfly/swarm/logging/LoggingProperties.java
+++ logging/api/src/main/java/org/wildfly/swarm/logging/LoggingProperties.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2016 Red Hat, Inc, and individual contributors.
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
