a7b99ed01043539e82db84c149ac772788c3d594
Ken Finnigan <ken@kenfinnigan.me>
2016-02-18 16:17:56 -0500

    Split out Keycloak pieces for extraction to external repo

---
+++ server/runtime/src/main/java/org/wildfly/swarm/keycloak/server/runtime/KeycloakServerConfiguration.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.keycloak.server.runtime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.dmr.ModelNode;
+import org.wildfly.swarm.container.runtime.AbstractServerConfiguration;
+import org.wildfly.swarm.keycloak.server.KeycloakServerFraction;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXTENSION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+
+/**
+ * @author Bob McWhirter
+ */
+public class KeycloakServerConfiguration extends AbstractServerConfiguration<KeycloakServerFraction> {
+
+    public KeycloakServerConfiguration() {
+        super(KeycloakServerFraction.class);
+    }
+
+    @Override
+    public KeycloakServerFraction defaultFraction() {
+        return new KeycloakServerFraction();
+    }
+
+    @Override
+    public List<ModelNode> getList(KeycloakServerFraction fraction) {
+        List<ModelNode> list = new ArrayList<>();
+
+        PathAddress address = PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, "keycloak-server"));
+
+        ModelNode node = new ModelNode();
+        node.get(OP_ADDR).set(EXTENSION, "org.keycloak.keycloak-wildfly-server-subsystem");
+        node.get(OP).set(ADD);
+        list.add(node);
+
+        node = new ModelNode();
+        node.get(OP_ADDR).set(address.toModelNode());
+        node.get(OP).set(ADD);
+        list.add(node);
+
+        return list;
+    }
+}
