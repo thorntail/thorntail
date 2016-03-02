4a3246160ec82a8e9253469f998e1cac9f992b48
Thomas Diesler <thomas.diesler@jboss.com>
2016-03-02 20:17:25 +0100

    [resolves #2] Add license files & update headers

--- core/test/src/test/java/org/wildfly/camel/swarm/core/subA/RouteBuilderA.java
+++ core/test/src/test/java/org/wildfly/camel/swarm/core/subA/RouteBuilderA.java
@@ -1,15 +1,15 @@
 /*
  * #%L
- * Wildfly Camel :: Testsuite
+ * Wildfly Swarm :: Camel Core Test
  * %%
- * Copyright (C) 2013 - 2014 RedHat
+ * Copyright (C) 2016 RedHat
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
