8e09b61341bdf06e2df9b54cdbbcbea865af33ea
Toby Crawley <toby@tcrawley.org>
2016-04-01 08:38:52 -0400

    Remove unused ruby script

--- src/main/ruby/publish_doc.rb
+++
@@ -1,8 +0,0 @@
-# maven-scm-publish-plugin feeds sh too many arguments, so we have to
-# do this ourselves
-
-git clone git@github.com:wildfly-swarm/wildfly-config-api.git target/scm-publish
-cd target/scm-publish && git checkout gh-pages
-rsync -avz api/target/site/ api/target/scm-publish
-cd api/target/scm-publish && git commit -a -m "CI generated API documentation"
-cd api/target/scm-publish && git push origin gh-pages
