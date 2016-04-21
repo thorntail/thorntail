de531849989b11f46f625dfc5f829ba10cfbbe38
Ken Finnigan <ken@kenfinnigan.me>
2016-04-20 21:49:44 -0400

    Update licenses and formatting

--- container/api/src/main/java/org/wildfly/swarm/container/internal/ProjectStageFactory.java
+++ container/api/src/main/java/org/wildfly/swarm/container/internal/ProjectStageFactory.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wildfly.swarm.container.internal;
 
 import java.io.IOException;
@@ -23,13 +38,13 @@ public class ProjectStageFactory {
             Iterable<Object> docs = yaml.loadAll(inputStream);
 
             for (Object item : docs) {
-                Map<String,Map<String,String>> doc = (Map<String,Map<String,String>>)item;
+                Map<String, Map<String, String>> doc = (Map<String, Map<String, String>>) item;
 
-                String stageName = doc.get(PROJECT_PREFIX)!=null ? doc.get(PROJECT_PREFIX).get(STAGE) : DEFAULT;
+                String stageName = doc.get(PROJECT_PREFIX) != null ? doc.get(PROJECT_PREFIX).get(STAGE) : DEFAULT;
                 ProjectStageImpl stage = new ProjectStageImpl(stageName);
 
                 for (String key : doc.keySet()) {
-                    if(!key.equals(PROJECT_PREFIX)) {
+                    if (!key.equals(PROJECT_PREFIX)) {
                         stage.getConfig().put(key, doc.get(key));
                     }
                 }
@@ -46,7 +61,7 @@ public class ProjectStageFactory {
                 //
             }
         }
-    };
+    }
 
     private static final String PROJECT_PREFIX = "project";
 
--- container/api/src/main/java/org/wildfly/swarm/container/internal/ProjectStageImpl.java
+++ container/api/src/main/java/org/wildfly/swarm/container/internal/ProjectStageImpl.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wildfly.swarm.container.internal;
 
 import java.util.HashMap;
@@ -13,8 +28,10 @@ import org.wildfly.swarm.spi.api.ProjectStage;
 public class ProjectStageImpl implements ProjectStage {
 
     private String name;
+
     private Map<String, Object> config = new HashMap<>();
-    private Map<String,String> properties = new HashMap<>();
+
+    private Map<String, String> properties = new HashMap<>();
 
     public ProjectStageImpl(String name) {
         this.name = name;
@@ -37,7 +54,7 @@ public class ProjectStageImpl implements ProjectStage {
                 String left = line.substring(0, pos);
 
                 // args precendence (java -Dfoo=bar)
-                if(null==System.getProperty(left))
+                if (null == System.getProperty(left))
                     this.properties.put(left, line.substring(pos + 1, line.length()));
                 else
                     this.properties.put(left, System.getProperty(left));
@@ -51,40 +68,39 @@ public class ProjectStageImpl implements ProjectStage {
     }
 
     @Override
-    public Map<String,String> getProperties() {
+    public Map<String, String> getProperties() {
         return this.properties;
     }
 
     private void parse(StringBuffer buffer, String parent, String key, Map<String, Object> config) {
 
-        if(parent!=null && buffer.substring(buffer.length()-1, buffer.length()).equals("\n"))
+        if (parent != null && buffer.substring(buffer.length() - 1, buffer.length()).equals("\n"))
             buffer.append(parent);
 
         buffer.append(key);
 
         Object o = config.get(key);
 
-        if(o instanceof Map) {
+        if (o instanceof Map) {
             // children: recursive
 
             buffer.append(".");
 
-            Map<String,Object> child = (Map<String,Object>)o;
+            Map<String, Object> child = (Map<String, Object>) o;
 
             String nextParent = buffer.toString();
             for (String childKey : child.keySet()) {
                 parse(buffer, nextParent, childKey, child);
             }
-        }
-        else if(o instanceof List)  {
+        } else if (o instanceof List) {
 
             // list values: non-recursive
 
-            List list = (List)o;
+            List list = (List) o;
             int i = 0;
             String prefix = buffer.toString();
             for (Object item : list) {
-                if(i==0)
+                if (i == 0)
                     buffer.append("[").append(i).append("]");
                 else
                     buffer.append(prefix).append("[").append(i).append("]");
@@ -92,9 +108,7 @@ public class ProjectStageImpl implements ProjectStage {
                 buffer.append("\n");
                 i++;
             }
-        }
-        else
-        {
+        } else {
             // simple values: non-recursive
             buffer.append("=").append(o);
             buffer.append("\n");
