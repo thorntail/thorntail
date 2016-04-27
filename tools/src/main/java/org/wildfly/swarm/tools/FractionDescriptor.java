0471f34fb658b5ac64ca4653e6468fd29cefd97b
George Gastaldi <gegastaldi@gmail.com>
2016-04-27 11:15:46 -0300

    Revert "SWARM-465: Undeprecated FractionDescriptor methods"
    This reverts commit 241768913fc0aeeec04ce8c447cea117a3ca31b6.

--- tools/src/main/java/org/wildfly/swarm/tools/FractionDescriptor.java
+++ tools/src/main/java/org/wildfly/swarm/tools/FractionDescriptor.java
@@ -65,14 +65,26 @@ public class FractionDescriptor {
         this.dependencies.add(dep);
     }
 
+    /**
+     * @deprecated Replaced with {@link #getGroupId()}
+     */
+    @Deprecated
     public String groupId() {
         return this.groupId;
     }
 
+    /**
+     * @deprecated Replaced with {@link #getArtifactId()}
+     */
+    @Deprecated
     public String artifactId() {
         return this.artifactId;
     }
 
+    /**
+     * @deprecated Replaced with {@link #getVersion()}
+     */
+    @Deprecated
     public String version() {
         return this.version;
     }
