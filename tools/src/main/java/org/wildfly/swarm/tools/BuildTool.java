3a815760b58e62aba3079d91e5c65cd6dd29c63b
George Gastaldi <gegastaldi@gmail.com>
2016-05-06 13:13:06 -0300

    SWARM-411: Provided and test dependencies should not be added to FractionUsageAnalyzer

--- tools/src/main/java/org/wildfly/swarm/tools/BuildTool.java
+++ tools/src/main/java/org/wildfly/swarm/tools/BuildTool.java
@@ -217,7 +217,9 @@ public class BuildTool {
         this.projectAsset.getArchive().as(ZipExporter.class).exportTo(tmpFile, true);
         final FractionUsageAnalyzer analyzer = new FractionUsageAnalyzer(this.fractionList)
                 .source(tmpFile);
-        this.dependencyManager.getDependencies().forEach(d -> analyzer.source(d.file));
+        this.dependencyManager.getDependencies().stream()
+                .filter(d -> !"provided".equals(d.scope) && !"test".equals(d.scope))
+                .forEach(d -> analyzer.source(d.file));
 
         final Set<FractionDescriptor> detectedFractions = analyzer.detectNeededFractions();
         this.log.info("Detected fractions: " + String.join(", ",
