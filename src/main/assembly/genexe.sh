4755ab54c52e82de57a469cf8ec2dffc89585f98
Toby Crawley <toby@tcrawley.org>
2016-05-16 14:47:38 -0400

    iQEcBAABCAAGBQJXOyVtAAoJEDZAhdNpr0OB0W0H/0ktyG786Sy16NnzlarTEEYq  UmurTeX9SSzUatIEsW5H50T3hHKY3LRwftJ3CSDG9Ng/tykulbSLHStAybzv5n/y  J3ID7/YSq8tMLBlUzS8qYKFlmxAnRkxwSA9K/IcKPV+/OwTNRZ+Vy7baS7E12NQ1  bjvG6v64biOFM9QVKI8csx2v0fzvgKpvdJ6f+g04RimmfXOBsdctJ5eepsmFAGzV  dBD21Z3kkDws+YViAYA0A7A5lJ528Z4oJv8hpMe2Uap7vMiH+54gHBGmBHUHx9HD  Lk7JgVpSAyIzG/1890ysejLMYqD7F4De082v9EGvZa03IEBn1UYUs13fCXTXNPw=  =3GyK  -----END PGP SIGNATURE-----
    Make swarmtool jar executable
    
    But only when building on !windows, since the tool we are using to do it
    is a sh script.
    
    This also improves the forward of the executable jar to attempt to find
    `java` and to honor `$JAVA_OPTS`.

--- src/main/assembly/genexe.sh
+++ src/main/assembly/genexe.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
-dir="target/bin"
-exe="$dir/swarmtool"
 
-if [ ! -d $dir ] ; then
-  mkdir -vp $dir
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 path-to-jar"
+    exit 1
 fi
 
-if [ -f $exe ] ; then 
-  echo "Removing old swarmtool executable"
-  rm -f $exe
-fi
+jar=$1
+tmp_jar="${jar}.tmp"
 
-echo "Creating swarmtool executable"
+echo "Making ${jar} executable"
 ARGS='"$@"'
 (echo '#!/bin/sh
-exec java -jar "$0" "$@"
-'; cat target/swarmtool-standalone.jar) > $exe && chmod +x $exe
+
+if [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
+    java="$JAVA_HOME/bin/java"
+elif type -p java > /dev/null 2>&1; then
+    java=$(type -p java)
+elif [[ -x "/usr/bin/java" ]];  then
+    java="/usr/bin/java"
+else
+    echo "Failed to find Java - please make sure it is in your path or set $JAVA_HOME"
+    exit 1
+fi
+
+export SWARMTOOL_NAME="$0"
+exec "$java" $JAVA_OPTS -jar "$0" "$@"
+'; cat $jar) > $tmp_jar
+
+mv $tmp_jar $jar
+chmod +x $jar
 
 
