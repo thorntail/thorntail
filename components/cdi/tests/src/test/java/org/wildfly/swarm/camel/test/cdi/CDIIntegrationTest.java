172c6e8ea69d0341fa2241cca7927bc956705c2b
Thomas Diesler <thomas.diesler@jboss.com>
2016-05-20 15:06:38 +0200

    [resolves #37] One maven module per fraction

---
+++ components/cdi/tests/src/test/java/org/wildfly/swarm/camel/test/cdi/CDIIntegrationTest.java
@@ -0,0 +1,146 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.camel.test.cdi;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.ServiceStatus;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.cdi.Uri;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.util.CamelContextHelper;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelContextRegistry;
+import org.wildfly.swarm.ContainerFactory;
+import org.wildfly.swarm.camel.core.CamelCoreFraction;
+import org.wildfly.swarm.camel.test.cdi.subA.Constants;
+import org.wildfly.swarm.camel.test.cdi.subA.RoutesContextA;
+import org.wildfly.swarm.camel.test.cdi.subA.RoutesContextB;
+import org.wildfly.swarm.camel.test.cdi.subA.RoutesContextC;
+import org.wildfly.swarm.camel.test.cdi.subA.RoutesContextD;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.spi.api.JARArchive;
+
+@RunWith(Arquillian.class)
+public class CDIIntegrationTest implements ContainerFactory {
+
+    @Resource(name = "java:jboss/camel/CamelContextRegistry")
+    CamelContextRegistry contextRegistry;
+
+    @Inject
+    @ContextName("contextA")
+    RoutesContextA routesA;
+    @Inject
+    @ContextName("contextB")
+    RoutesContextB routesB;
+    @Inject
+    @ContextName("contextC")
+    RoutesContextC routesC;
+    @Inject
+    @ContextName("contextD")
+    RoutesContextD routesD;
+
+    @Inject
+    @Uri(value = "seda:foo", context = "contextD")
+    ProducerTemplate producerD;
+
+    @Deployment
+    public static JARArchive deployment() {
+        JARArchive archive = ShrinkWrap.create(JARArchive.class, "camel-cdi-tests.jar");
+        archive.addPackage(RoutesContextA.class.getPackage());
+        archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return archive;
+    }
+
+    @Override
+    public Container newContainer(String... args) throws Exception {
+        return new Container().fraction(new CamelCoreFraction());
+    }
+
+    @Test
+    public void checkContextsHaveCorrectEndpointsAndRoutes() throws Exception {
+
+        CamelContext contextA = assertCamelContext("contextA");
+        assertHasEndpoints(contextA, "seda://A.a", "mock://A.b");
+
+        MockEndpoint mockEndpoint = routesA.b;
+        mockEndpoint.expectedBodiesReceived(Constants.EXPECTED_BODIES_A);
+        routesA.sendMessages();
+        mockEndpoint.assertIsSatisfied();
+
+        CamelContext contextB = assertCamelContext("contextB");
+        assertHasEndpoints(contextB, "seda://B.a", "mock://B.b");
+
+        MockEndpoint mockEndpointB = routesB.b;
+        mockEndpointB.expectedBodiesReceived(Constants.EXPECTED_BODIES_B);
+        routesB.sendMessages();
+        mockEndpointB.assertIsSatisfied();
+
+        // lets check the routes where we default the context from the @ContextName
+        CamelContext contextC = assertCamelContext("contextC");
+        assertHasEndpoints(contextC, "seda://C.a", "mock://C.b");
+
+        MockEndpoint mockEndpointC = routesC.b;
+        mockEndpointC.expectedBodiesReceived(Constants.EXPECTED_BODIES_C);
+        routesC.sendMessages();
+        mockEndpointC.assertIsSatisfied();
+
+        CamelContext contextD = assertCamelContext("contextD");
+        assertHasEndpoints(contextD, "seda://D.a", "mock://D.b");
+
+        MockEndpoint mockEndpointD = routesD.b;
+        mockEndpointD.expectedBodiesReceived(Constants.EXPECTED_BODIES_D);
+        routesD.sendMessages();
+        mockEndpointD.assertIsSatisfied();
+
+        CamelContext contextE = assertCamelContext("contextD");
+        assertHasEndpoints(contextE, "seda://D.a", "mock://D.b");
+        MockEndpoint mockDb = CamelContextHelper.getMandatoryEndpoint(contextE, "mock://D.b", MockEndpoint.class);
+        mockDb.reset();
+        mockDb.expectedBodiesReceived(Constants.EXPECTED_BODIES_D_A);
+        for (Object body : Constants.EXPECTED_BODIES_D_A) {
+            producerD.sendBody("seda:D.a", body);
+        }
+        mockDb.assertIsSatisfied();
+    }
+
+    private void assertHasEndpoints(CamelContext context, String... uris) {
+        Map<String, Endpoint> endpointMap = context.getEndpointMap();
+        for (String uri : uris) {
+            Endpoint endpoint = endpointMap.get(uri);
+            Assert.assertNotNull("CamelContext " + context + " does not have an Endpoint with URI " + uri + " but has " + endpointMap.keySet(), endpoint);
+        }
+    }
+
+    private CamelContext assertCamelContext(String contextName) {
+        CamelContext camelctx = contextRegistry.getCamelContext(contextName);
+        Assert.assertEquals(ServiceStatus.Started, camelctx.getStatus());
+        return camelctx;
+    }
+}
---
+++ components/cxf-rs/tests/src/test/java/org/wildfly/swarm/camel/test/jaxrs/JAXRSProducerTest.java
@@ -0,0 +1,84 @@
+/*
+ * #%L
+ * Wildfly Swarm :: Camel Web Test
+ * %%
+ * Copyright (C) 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.swarm.camel.test.jaxrs;
+
+import java.net.MalformedURLException;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+import org.wildfly.swarm.ContainerFactory;
+import org.wildfly.swarm.camel.core.CamelCoreFraction;
+import org.wildfly.swarm.camel.test.jaxrs.subA.GreetingService;
+import org.wildfly.swarm.camel.test.jaxrs.subA.RestApplication;
+import org.wildfly.swarm.container.Container;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class JAXRSProducerTest implements ContainerFactory {
+
+    @Deployment
+    public static WebArchive deployment() {
+        final WebArchive archive = ShrinkWrap.create(WebArchive.class, "jaxrs-endpoint.war");
+        archive.addPackage(RestApplication.class.getPackage());
+        return archive;
+    }
+
+    @Override
+    public Container newContainer(String... args) throws Exception {
+        return new Container().fraction(new CamelCoreFraction());
+    }
+
+    @Test
+    public void testJaxrsProducer() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .setHeader(Exchange.HTTP_METHOD, constant("GET")).
+                to("cxfrs://" + getEndpointAddress("/") + "?resourceClasses=" + GreetingService.class.getName());
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            String result = producer.requestBodyAndHeader("direct:start", "mybody", "name", "Kermit", String.class);
+            Assert.assertEquals("Hello Kermit", result);
+        } finally {
+            camelctx.stop();
+        }
+    }
+
+    private String getEndpointAddress(String contextPath) throws MalformedURLException {
+        return "http://localhost:8080" + contextPath + "/rest/greet/hello/Kermit";
+    }
+}
