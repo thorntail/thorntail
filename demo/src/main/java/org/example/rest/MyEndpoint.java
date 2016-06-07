537968f74607819420b9fac039b31e648a303ac2
George Gastaldi <gegastaldi@gmail.com>
2016-06-07 15:24:13 -0300

    Enabled @Resource

--- demo/src/main/java/org/example/rest/MyEndpoint.java
+++ demo/src/main/java/org/example/rest/MyEndpoint.java
@@ -1,5 +1,7 @@
 package org.example.rest;
 
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
 import javax.naming.InitialContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,18 +11,18 @@ import javax.ws.rs.core.Response;
 import io.vertx.resourceadapter.VertxConnectionFactory;
 
 @Path("/my")
-//@Stateless
+@Stateless
 public class MyEndpoint {
 
    //java.lang.ClassCastException: io.vertx.resourceadapter.impl.VertxConnectionFactoryImpl cannot be cast to io.vertx.resourceadapter.VertxConnectionFactory
-//   @Resource(mappedName = "java:/eis/VertxConnectionFactory")
-//	VertxConnectionFactory connectionFactory;
+   @Resource(mappedName = "java:/eis/VertxConnectionFactory")
+	VertxConnectionFactory connectionFactory;
 
 	@GET
 	@Produces("text/plain")
 	public Response doGet() throws Exception {
       //FIXME: java.lang.ClassCastException: io.vertx.resourceadapter.impl.VertxConnectionFactoryImpl cannot be cast to io.vertx.resourceadapter.VertxConnectionFactory
-      Object connectionFactory = new InitialContext().lookup("java:/eis/VertxConnectionFactory");
+//      Object connectionFactory = new InitialContext().lookup("java:/eis/VertxConnectionFactory");
       return Response.ok("Lookup: "+connectionFactory).build();
 	}
 }
