376d94efbf8d2c89821c5174de19794784ca45eb
George Gastaldi <gegastaldi@gmail.com>
2016-06-07 01:17:04 -0300

    Detected CCE in tests

--- demo/src/main/java/org/example/rest/MyEndpoint.java
+++ demo/src/main/java/org/example/rest/MyEndpoint.java
@@ -1,22 +1,26 @@
 package org.example.rest;
 
-import javax.annotation.Resource;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import javax.naming.InitialContext;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
-import io.vertx.core.Vertx;
 import io.vertx.resourceadapter.VertxConnectionFactory;
 
 @Path("/my")
+//@Stateless
 public class MyEndpoint {
-	@Resource(name = "java:/eis/VertxConnectionFactory")
-	VertxConnectionFactory connectionFactory;
+
+   //java.lang.ClassCastException: io.vertx.resourceadapter.impl.VertxConnectionFactoryImpl cannot be cast to io.vertx.resourceadapter.VertxConnectionFactory
+//   @Resource(mappedName = "java:/eis/VertxConnectionFactory")
+//	VertxConnectionFactory connectionFactory;
 
 	@GET
 	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("Lookup: "+connectionFactory).build();
+	public Response doGet() throws Exception {
+      //FIXME: java.lang.ClassCastException: io.vertx.resourceadapter.impl.VertxConnectionFactoryImpl cannot be cast to io.vertx.resourceadapter.VertxConnectionFactory
+      Object connectionFactory = new InitialContext().lookup("java:/eis/VertxConnectionFactory");
+      return Response.ok("Lookup: "+connectionFactory).build();
 	}
 }
