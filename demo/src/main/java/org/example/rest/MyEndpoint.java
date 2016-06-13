3aef5d6cef33d5ed2563a8131c53b0d18a9a98b5
George Gastaldi <gegastaldi@gmail.com>
2016-06-12 22:40:41 -0300

    More debug info

--- demo/src/main/java/org/example/rest/MyEndpoint.java
+++ demo/src/main/java/org/example/rest/MyEndpoint.java
@@ -2,6 +2,7 @@ package org.example.rest;
 
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.naming.InitialContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,13 +15,18 @@ import io.vertx.resourceadapter.VertxConnectionFactory;
 @Stateless
 public class MyEndpoint {
 
-    @Resource(mappedName = "java:/eis/VertxConnectionFactory")
+    private static final String JNDI_NAME = "java:/eis/VertxConnectionFactory";
+
+    @Resource(mappedName = JNDI_NAME)
     VertxConnectionFactory connectionFactory;
 
     @GET
     @Produces("text/plain")
     public Response doGet() throws Exception {
-        System.out.println(Vertx.vertx());
-        return Response.ok("Lookup: " + connectionFactory).build();
+        Object obj = new InitialContext().lookup(JNDI_NAME);
+        System.out.println(connectionFactory instanceof VertxConnectionFactory);
+        System.out.println("VERTX: "+Vertx.class.getClassLoader());
+        System.out.println("VCF: "+VertxConnectionFactory.class.getClassLoader());
+        return Response.ok("Lookup: " + obj.getClass().getClassLoader()).build();
     }
 }
