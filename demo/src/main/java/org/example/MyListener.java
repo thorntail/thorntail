ef7b5ac48979ec9cb9b77587527011ec64a62799
George Gastaldi <gegastaldi@gmail.com>
2016-06-13 17:19:30 -0300

    Enabled MDB

--- demo/src/main/java/org/example/MyListener.java
+++ demo/src/main/java/org/example/MyListener.java
@@ -10,14 +10,14 @@ import org.jboss.ejb3.annotation.ResourceAdapter;
 /**
  * Created by ggastald on 13/06/16.
  */
-//@MessageDriven(name = "VertxMonitor",
-//        messageListenerInterface = VertxListener.class,
-//        activationConfig = {
-//                @ActivationConfigProperty(propertyName = "address", propertyValue = "tacos"),
-//                @ActivationConfigProperty(propertyName = "clusterHost", propertyValue = "localhost"),
-//                @ActivationConfigProperty(propertyName = "clusterPort", propertyValue = "0"),
-//        })
-//@ResourceAdapter("vertx-ra")
+@MessageDriven(name = "VertxMonitor",
+        messageListenerInterface = VertxListener.class,
+        activationConfig = {
+                @ActivationConfigProperty(propertyName = "address", propertyValue = "tacos"),
+                @ActivationConfigProperty(propertyName = "clusterHost", propertyValue = "localhost"),
+                @ActivationConfigProperty(propertyName = "clusterPort", propertyValue = "0"),
+        })
+@ResourceAdapter("vertx-ra")
 public class MyListener implements VertxListener{
 
     @Override
