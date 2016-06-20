aff69934e7d7f3a2ad585bbb069ba1564ac2afab
George Gastaldi <gegastaldi@gmail.com>
2016-06-20 18:03:28 -0300

    SWARM-258: Adds JDR fraction

---
+++ jdr/api/src/main/java/org/wildfly/swarm/jdr/JdrFraction.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.jdr;
+
+import org.wildfly.swarm.config.Jdr;
+import org.wildfly.swarm.spi.api.Fraction;
+import org.wildfly.swarm.spi.api.annotations.Configuration;
+
+/**
+ * @author George Gastaldi
+ */
+@Configuration(
+        marshal = true,
+        extension = "org.jboss.as.jdr",
+        parserFactoryClassName = "org.wildfly.swarm.jdr.runtime.JdrParserFactory"
+)
+
+public class JdrFraction extends Jdr<JdrFraction> implements Fraction {
+    public JdrFraction() {
+    }
+}
