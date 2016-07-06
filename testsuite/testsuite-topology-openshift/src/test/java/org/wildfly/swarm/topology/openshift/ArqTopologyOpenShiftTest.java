daa20bbe3f458d769631fb627214692bcc2a776e
Bob McWhirter <bob@mcwhirter.org>
2016-07-06 12:52:52 -0400

    Smoosh topology-openshift fraction.

---
+++ testsuite/testsuite-topology-openshift/src/test/java/org/wildfly/swarm/topology/openshift/ArqTopologyOpenShiftTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.topology.openshift;
+
+import org.fest.assertions.Assertions;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.ContainerFactory;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.spi.api.JARArchive;
+import org.wildfly.swarm.topology.Topology;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Bob McWhirter
+ */
+@RunWith(Arquillian.class)
+@Ignore
+public class ArqTopologyOpenShiftTest implements ContainerFactory {
+
+    @Deployment
+    public static Archive createDeployment() {
+        JARArchive deployment = ShrinkWrap.create(JARArchive.class);
+        deployment.add(EmptyAsset.INSTANCE, "nothing");
+        deployment.addPackage(Assertions.class.getPackage());
+        return deployment;
+    }
+
+    @Override
+    public Container newContainer(String... args) throws Exception {
+        return new Container().fraction(new OpenShiftTopologyFraction());
+    }
+
+    @Test
+    public void testIt() throws Exception {
+        Topology topology = Topology.lookup();
+        assertThat(topology).isNotNull();
+    }
+
+}
