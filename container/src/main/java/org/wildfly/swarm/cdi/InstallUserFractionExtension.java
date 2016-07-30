a848a8046221f0518f2fc96f88b4aeeaff01ac0a
Ken Finnigan <ken@kenfinnigan.me>
2016-07-30 01:09:43 -0400

    Better handling of adding the qualifiers onto the custom Fraction bean.
    Also ensure we don't add @DefaultFraction, @ApplicationScoped and @Singleton into the list of Qualifiers of the new bean

--- container/src/main/java/org/wildfly/swarm/cdi/InstallUserFractionExtension.java
+++ container/src/main/java/org/wildfly/swarm/cdi/InstallUserFractionExtension.java
@@ -1,9 +1,26 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wildfly.swarm.cdi;
 
-import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
@@ -21,12 +38,24 @@ import org.wildfly.swarm.spi.api.Fraction;
 public class InstallUserFractionExtension implements Extension {
     public static Map<Class<?>, Fraction> userFractions = new HashMap<>();
 
+    /**
+     * Find all Fraction types with @DefaultFraction that are discovered by Weld and veto those
+     * where we have a custom Fraction that was added.
+     *
+     * @param pat ProcessAnnotatedType
+     */
     <T> void processAnnotatedType(@Observes @WithAnnotations(DefaultFraction.class) ProcessAnnotatedType<T> pat) {
         if (userFractions.containsKey(pat.getAnnotatedType().getJavaClass())) {
             pat.veto();
         }
     }
 
+    /**
+     * Once all beans have been discovered by Weld, for each custom fraction that we have,
+     * add the Bean instance to Weld as a replacement for the @DefaultFraction instance we vetoed.
+     *
+     * @param abd AfterBeanDiscovery
+     */
     void afterBeanDiscovery(@Observes AfterBeanDiscovery abd) {
         for (Fraction fraction : userFractions.values()) {
             BeanConfigurator<Object> configurator = abd.addBean()
@@ -35,14 +64,14 @@ public class InstallUserFractionExtension implements Extension {
                     .scope(Singleton.class)
                     .produceWith(() -> fraction);
 
-            Annotation[] qualifiers = new Annotation[fraction.getClass().getAnnotations().length - 1];
-            int index = 0;
-            for (Annotation annotation : fraction.getClass().getAnnotations()) {
-                if (!annotation.annotationType().equals(DefaultFraction.class)) {
-                    qualifiers[index++] = annotation;
-                }
-            }
-            configurator.qualifiers(qualifiers);
+            configurator.qualifiers(
+                    Arrays.asList(fraction.getClass().getAnnotations())
+                            .stream()
+                            .filter(p -> (!p.annotationType().equals(DefaultFraction.class)
+                                    && !p.annotationType().equals(ApplicationScoped.class)
+                                    && !p.annotationType().equals(Singleton.class)))
+                            .collect(Collectors.toSet())
+            );
         }
     }
 }
