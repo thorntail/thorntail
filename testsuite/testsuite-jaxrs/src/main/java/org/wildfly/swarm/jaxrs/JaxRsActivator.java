5d4c0870114d924ccbba06da498ed0f4ff56317f
Heiko Braun <ike.braun@googlemail.com>
2016-08-24 11:52:22 +0200

    SWARM-588: HealthCheck endpoints support @ApplicationPath annotations
    Motivation
    ----------
    Fix aproblem with @ApplicationPath declarations in JAX-RS depyments that are used as health check resources
    
    Modifications
    -------------
    Change meta data poarser to respect @ApplicationPath annotations
    
    Result
    ------
    You can now rely on @ApplictionPath to declare the top level entry point ot health check resources in a given deplyment

---
+++ testsuite/testsuite-jaxrs/src/main/java/org/wildfly/swarm/jaxrs/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package org.wildfly.swarm.jaxrs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/webcontext")
+public class JaxRsActivator extends Application {
+}
