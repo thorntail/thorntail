5f434bd876746906b235367a17ad98db6edcaaa9
Bob McWhirter <bob@mcwhirter.org>
2016-09-09 11:50:42 -0400

    SWARM-664 - API for jolokia-access.xml (#130)
    Motivation
    ----------
    
    Having an open and unsecured jolokia end-point is potentially
    dangerous.
    
    Modifications
    -------------
    
    Provided several methods for creating/using a jolokia-access.xml,
    along with general configuration of the deployed jolokia.war.
    
    The JolokiaFraction supports prepareJolokiaWar(Consumer<Archive>)
    which can be used for any random customization of the deployed
    jolokia.war.
    
    Additionally, several static methods exist on JolokiaFraction
    that producer Consumer<Archive> that know how to configure
    jolokia-access.xml based upon a URL, a File, or the JolokiaAccess
    API.
    
    In all cases, once a jolokia-access.xml has been set, further
    setting is disallowed.  The deployment-producer also sets up,
    as the head of any Consumer<Archive>, a jolokia-access.xml
    preparer that knows how to check the swarm.jolokia.access.xml
    configuration value, therefore preferring that to any subsequent,
    user-installed, programatic preparers that muck with
    jolokia-access.xml.
    
    Result
    ------
    
    You can now secure Jolokia using Jolokia's own mechanisms, for
    some small values of "secure".

---
+++ jolokia/src/main/java/org/wildfly/swarm/jolokia/access/MBeanRule.java
@@ -0,0 +1,55 @@
+package org.wildfly.swarm.jolokia.access;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** An MBean rule.
+ *
+ * @author Bob McWhirter
+ */
+public class MBeanRule {
+
+    public interface Consumer extends java.util.function.Consumer<MBeanRule> {
+
+    }
+
+    MBeanRule(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    /** An operation name or pattern.
+     *
+     * @param operation The operation.
+     * @return This rule.
+     */
+    public MBeanRule operation(String operation) {
+        this.operations.add( operation );
+        return this;
+    }
+
+    public List<String> operations() {
+        return this.operations;
+    }
+
+    /** An attribute name or pattern.
+     *
+     * @param attribute The attribute.
+     * @return This rule.
+     */
+    public MBeanRule attribute(String attribute) {
+        this.attributes.add( attribute );
+        return this;
+    }
+
+    public List<String> attributes() {
+        return this.attributes;
+    }
+
+    private String name;
+    private List<String> operations = new ArrayList<>();
+    private List<String> attributes = new ArrayList<>();
+}
