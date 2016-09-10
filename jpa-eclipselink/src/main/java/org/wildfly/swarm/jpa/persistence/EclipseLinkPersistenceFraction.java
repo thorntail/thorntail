cf3e7e33a5f631ffabdb3bc51d7a5affe0476952
Slobodan Lazarevski <slobodanl@gmail.com>
2016-09-10 23:48:27 +0200

    EclipseLink as JPA provider (#138)

---
+++ jpa-eclipselink/src/main/java/org/wildfly/swarm/jpa/persistence/EclipseLinkPersistenceFraction.java
@@ -0,0 +1,11 @@
+package org.wildfly.swarm.jpa.persistence;
+
+import org.wildfly.swarm.spi.api.Fraction;
+import org.wildfly.swarm.spi.api.annotations.DeploymentModule;
+
+/**
+ * Created by slobolai on 9/8/2016.
+ */
+@DeploymentModule(name="org.eclipse.persistence")
+public class EclipseLinkPersistenceFraction implements Fraction<EclipseLinkPersistenceFraction> {
+}
