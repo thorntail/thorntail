456c700e62c54907c1926fab1f6e680b996e25cf
Slobodan Lazarevski <slobolai@axis.com>
2016-09-12 10:34:05 +0200

    EclipseLink module fix
    - Dependency with EclipseLink
    - Removed private signatures, replaced with RH license

--- jpa-eclipselink/src/main/java/org/wildfly/swarm/jpa/persistence/EclipseLinkPersistenceFraction.java
+++ jpa-eclipselink/src/main/java/org/wildfly/swarm/jpa/persistence/EclipseLinkPersistenceFraction.java
@@ -1,11 +1,24 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wildfly.swarm.jpa.persistence;
 
 import org.wildfly.swarm.spi.api.Fraction;
 import org.wildfly.swarm.spi.api.annotations.DeploymentModule;
 
-/**
- * Created by slobolai on 9/8/2016.
- */
+
 @DeploymentModule(name="org.eclipse.persistence")
 public class EclipseLinkPersistenceFraction implements Fraction<EclipseLinkPersistenceFraction> {
 }
