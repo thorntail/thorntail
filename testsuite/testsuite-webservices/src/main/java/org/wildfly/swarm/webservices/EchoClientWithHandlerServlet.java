f48cb0178be19333b4acc97d60f3d8b79ce95c33
Ken Finnigan <ken@kenfinnigan.me>
2016-09-30 14:54:43 -0400

    SWARM-582: saaj-impl module not found
    Motivation
    ----------
    Within the webservices subsystem of WildFly it directly loads this module when it's unable to find a service entry for javax.xml.soap.MetaFactory.
    
    As its directly loading it, our plugin doesn't have a way to know it needs that module present so we need to specifically reference it in our build.
    
    Modifications
    -------------
    Added a test showing the problem and then updated module.conf for webservices fraction to include saaj-impl
    
    Result
    ------
    Solves exception when trying to lookup a MetaFactory for SOAP.

---
+++ testsuite/testsuite-webservices/src/main/java/org/wildfly/swarm/webservices/EchoClientWithHandlerServlet.java
@@ -0,0 +1,33 @@
+package org.wildfly.swarm.webservices;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+
+/**
+ * @author Ken Finnigan
+ */
+@WebServlet("/clientWithHandler")
+public class EchoClientWithHandlerServlet extends HttpServlet {
+
+    @WebServiceRef(EchoServiceClientWithHandler.class)
+    private EchoService echoService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String input = req.getParameter("message");
+        final String echo = echoService.echo(input);
+
+        resp.setContentType("text/plain");
+        final PrintWriter writer = resp.getWriter();
+        writer.write(echo);
+        writer.flush();
+        writer.close();
+    }
+}
---
+++ testsuite/testsuite-webservices/src/main/java/org/wildfly/swarm/webservices/EchoService.java
@@ -0,0 +1,19 @@
+package org.wildfly.swarm.webservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * @author Ken Finnigan
+ */
+@WebService
+@SOAPBinding
+public interface EchoService {
+    String NAMESPACE = "webservices.swarm.wildfly.org";
+    String SERVICE_NAME = "echoService";
+
+    @WebMethod
+    String echo(@WebParam(name = "input") final String input);
+}
