2eed5951bc3d7a6b98d22a5f7f24116f52125b90
Ken Finnigan <ken@kenfinnigan.me>
2016-10-06 12:40:46 -0400

    SWARM-756: Restructure project modules
    Motivation
    ----------
    It has become increasingly difficult to know what the different modules of the project provide, so its necessary to add some delineation between areas
    
    Modifications
    -------------
    We now have /core for the main runtime setup pieces.
    /fractions contains all the fractions, which is further subdivided into
    /fractions/javaee
    /fractions/netflix
    /fractions/camel
    /fractions/cdi-extensions
    
    As part of this cdi-ext has moved to /fractions/cdi-extensions/cdi-config and been renamed to cdi-config.
    
    Result
    ------
    No impact on runtime behavior, simply a re-org of pieces for development

---
+++ fractions/cdi-extensions/cdi-config/src/main/java/org/wildfly/swarm/cdi/config/runtime/InjectStageConfigExtensionFactory.java
@@ -0,0 +1,28 @@
+package org.wildfly.swarm.cdi.config.runtime;
+
+import java.lang.reflect.Field;
+
+import javax.inject.Inject;
+
+import org.jboss.modules.Module;
+import org.jboss.modules.ModuleIdentifier;
+import org.wildfly.swarm.spi.api.StageConfig;
+import org.wildfly.swarm.spi.api.UserSpaceExtensionFactory;
+
+/**
+ * @author Bob McWhirter
+ */
+public class InjectStageConfigExtensionFactory implements UserSpaceExtensionFactory {
+
+    @Inject
+    StageConfig stageConfig;
+
+    @Override
+    public void configure() throws Exception {
+        Module module = Module.getBootModuleLoader().loadModule(ModuleIdentifier.create("org.wildfly.swarm.cdi.config", "deployment"));
+        Class<?> use = module.getClassLoader().loadClass("org.wildfly.swarm.cdi.config.deployment.InjectStageConfigExtension");
+        Field field = use.getDeclaredField("stageConfig");
+        field.setAccessible(true);
+        field.set(null, this.stageConfig);
+    }
+}
