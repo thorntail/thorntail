a9254c712cb8d01122dce95957fe9b76bc430f73
George Gastaldi <gegastaldi@gmail.com>
2016-10-21 11:14:08 -0200

    SWARM-478: Add Flyway fraction (#188)
    Motivation
    ----------
    Flyway (https://flywaydb.org/) is an open-source database migration tool. It strongly favors simplicity and convention over configuration.
    
    Modifications
    -------------
    Created a flyway fraction that automatically bounds to the primary DataSource if not configured or uses the provided URL/user/pass otherwise.
    It registers a ServletContextListener in the deployed app to perform the migration. Scripts must be placed under src/main/resources/db/migration
    
    Result
    ------
    Flyway can now be activated in Swarm applications by simply adding the flyway fraction and dropping the scripts in the specified place.

---
+++ fractions/flyway/src/main/java/org/wildfly/swarm/flyway/FlywayFraction.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.flyway;
+
+import org.wildfly.swarm.spi.api.Fraction;
+import org.wildfly.swarm.spi.api.annotations.DeploymentModule;
+
+/**
+ * Flyway Fraction
+ *
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@DeploymentModule(name = "org.wildfly.swarm.flyway", slot = "deployment")
+public class FlywayFraction implements Fraction<FlywayFraction> {
+
+    /**
+     * Uses the specified connection info if not <code>null</code>. Otherwise
+     * use primary Datasource
+     */
+    private String jdbcUrl;
+    private String jdbcUser;
+    private String jdbcPassword;
+
+    public String jdbcPassword() {
+        return jdbcPassword;
+    }
+
+    public String jdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public String jdbcUser() {
+        return jdbcUser;
+    }
+
+    public FlywayFraction jdbcPassword(String jdbcPassword) {
+        this.jdbcPassword = jdbcPassword;
+        return this;
+    }
+
+    public FlywayFraction jdbcUser(String jdbcUser) {
+        this.jdbcUser = jdbcUser;
+        return this;
+    }
+
+    public FlywayFraction jdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+        return this;
+    }
+
+    public boolean usePrimaryDataSource() {
+        return this.jdbcPassword == null;
+    }
+}
---
+++ fractions/flyway/src/main/java/org/wildfly/swarm/flyway/deployment/FlywayMigrationServletContextListener.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.flyway.deployment;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.inject.Vetoed;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+
+/**
+ * A ServletContextListener implementation that performs DB migration with
+ * Flyway
+ *
+ * @author @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@Vetoed
+@WebListener
+public class FlywayMigrationServletContextListener implements ServletContextListener {
+
+    public static final String FLYWAY_JNDI_DATASOURCE = "flyway.jndi.datasource";
+    public static final String FLYWAY_JDBC_PASSWORD = "flyway.jdbc.password";
+    public static final String FLYWAY_JDBC_USER = "flyway.jdbc.user";
+    public static final String FLYWAY_JDBC_URL = "flyway.jdbc.url";
+
+    private static final Logger logger = Logger.getLogger(FlywayMigrationServletContextListener.class.getName());
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext sc = sce.getServletContext();
+        Flyway flyway = new Flyway();
+        String dataSourceJndi = sc.getInitParameter(FLYWAY_JNDI_DATASOURCE);
+        if (dataSourceJndi != null) {
+            try {
+                DataSource dataSource = (DataSource) new InitialContext().lookup(dataSourceJndi);
+                flyway.setDataSource(dataSource);
+            } catch (NamingException ex) {
+                logger.log(Level.SEVERE, "Error while looking up DataSource", ex);
+                // Do not proceed
+                return;
+            }
+        } else {
+            String url = sc.getInitParameter(FLYWAY_JDBC_URL);
+            String user = sc.getInitParameter(FLYWAY_JDBC_USER);
+            String password = sc.getInitParameter(FLYWAY_JDBC_PASSWORD);
+            flyway.setDataSource(url, user, password);
+        }
+        flyway.migrate();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
