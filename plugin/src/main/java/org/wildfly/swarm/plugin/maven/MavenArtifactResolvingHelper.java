03636042a930633f3eca13bdb9408deffa053a7b
George Gastaldi <gegastaldi@gmail.com>
2016-10-26 13:30:00 -0200

    Revert "SWARM-791: Use Artifact.getBaseVersion() to avoid crazy snapshot versions"
    This reverts commit 59c47aa10240f9178fce9356f0911832a9ecc584.

--- plugin/src/main/java/org/wildfly/swarm/plugin/maven/MavenArtifactResolvingHelper.java
+++ plugin/src/main/java/org/wildfly/swarm/plugin/maven/MavenArtifactResolvingHelper.java
@@ -153,7 +153,7 @@ public class MavenArtifactResolvingHelper implements ArtifactResolvingHelper {
                     return new ArtifactSpec(node.getDependency().getScope(),
                             artifact.getGroupId(),
                             artifact.getArtifactId(),
-                            artifact.getBaseVersion(),
+                            artifact.getVersion(),
                             artifact.getExtension(),
                             artifact.getClassifier(),
                             artifact.getFile());
