07818a9ba44a055eab159eadab93ab4b211449e4
emag <lanabe.lanabe@gmail.com>
2016-12-10 15:19:40 +0900

    Remove tests because the place adds keycloak.json moved to ArchivePreparer, it should be tested with Arq.

--- fractions/keycloak/src/test/java/org/wildfly/swarm/keycloak/SecuredTest.java
+++ fractions/keycloak/src/test/java/org/wildfly/swarm/keycloak/SecuredTest.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.Node;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.Asset;
@@ -15,7 +14,6 @@ import org.junit.Test;
 import org.wildfly.swarm.undertow.WARArchive;
 
 import static org.fest.assertions.Assertions.assertThat;
-import static org.junit.Assert.fail;
 
 /**
  * @author Bob McWhirter
@@ -43,45 +41,6 @@ public class SecuredTest {
 
         assertThat( lines ).contains( "<servlet-name>comingsoon</servlet-name>" );
         assertThat( lines ).contains( "<url-pattern>/cheddar</url-pattern>" );
-    }
-
-    @Test
-    public void testKeycloakJsonFromClasspath() throws Exception {
-        WARArchive archive = ShrinkWrap.create(WARArchive.class);
-        archive.as(Secured.class)
-                .protect("/cheddar");
-
-        String keycloakJson = extractKeycloakJson(archive);
-        assertThat(keycloakJson).contains("http://localhost:9090/auth");
-    }
-
-    @Test
-    public void testOverriddenByExternalKeycloakJson() throws Exception {
-        System.setProperty("swarm.keycloak.json.path", "src/test/resources/external-keycloak.json");
-
-        WARArchive archive = ShrinkWrap.create(WARArchive.class);
-        archive.as(Secured.class)
-                .protect("/cheddar");
-
-        String keycloakJson = extractKeycloakJson(archive);
-        assertThat(keycloakJson).contains("http://localhost:19090/auth");
-
-        System.clearProperty("swarm.keycloak.json.path");
-    }
-
-    private static String extractKeycloakJson(Archive archive) throws Exception {
-        String keycloakPath = "WEB-INF/keycloak.json";
-        Node keycloakJson = archive.get(keycloakPath);
-        if (keycloakJson == null) {
-            fail();
-        }
-
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader =
-                     new BufferedReader(new InputStreamReader(keycloakJson.getAsset().openStream()))) {
-            reader.lines().forEach(sb::append);
-            return sb.toString();
-        }
     }
 
 }
