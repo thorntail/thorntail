a9ff85b7c2a5d19025c695d3c9a38ef298a5d25b
Bob McWhirter <bob@mcwhirter.org>
2017-01-10 20:10:05 -0500

    SWARM-689 - Upgrade Keycloak (to 2.5.0.Final) (#294)
    Motivation
    ----------
    
    Keycloak has moved into the future, like Future Man who
    plays the weird instruments with Bela Fleck and the Flecktones.
    We have lagged behind, like a lag-screw.
    
    We should remain modern.
    
    Modifications
    -------------
    
    In a parallel universe, we now generate keycloak-config-api.
    We're replaced our fraction which deals with keycloak-server.json
    to now deal with DMR and the generated config-api.
    
    Additionally, we manage the outboard keycloak-themes artifact
    and asplode them into a temporary directory for usage.
    
    Result
    ------
    
    Newer keycloak.

---
+++ fractions/keycloak-server/src/main/java/org/wildfly/swarm/keycloak/server/runtime/KeycloakCacheCustomizer.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.keycloak.server.runtime;
+
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.wildfly.swarm.config.infinispan.CacheContainer;
+import org.wildfly.swarm.config.infinispan.cache_container.EvictionComponent;
+import org.wildfly.swarm.infinispan.InfinispanFraction;
+import org.wildfly.swarm.spi.api.Customizer;
+import org.wildfly.swarm.spi.runtime.annotations.Post;
+
+/**
+ * @author Bob McWhirter
+ */
+@Post
+@Singleton
+public class KeycloakCacheCustomizer implements Customizer {
+
+    @Inject
+    @Any
+    private InfinispanFraction infinispan;
+
+
+    @Override
+    public void customize() {
+
+        CacheContainer cache = infinispan.subresources().cacheContainer("keycloak");
+        if (cache == null) {
+            infinispan.cacheContainer("keycloak", (c) -> c.jndiName("infinispan/Keycloak")
+                    .localCache("realms", (localCache) -> {
+                        localCache.evictionComponent((evict) -> {
+                            evict.maxEntries(10000L);
+                            evict.strategy(EvictionComponent.Strategy.LRU);
+                        });
+                    })
+                    .localCache("users", (localCache) -> {
+                        localCache.evictionComponent((evict) -> {
+                            evict.maxEntries(10000L);
+                            evict.strategy(EvictionComponent.Strategy.LRU);
+                        });
+                    })
+                    .localCache("sessions")
+                    .localCache("offlineSessions")
+                    .localCache("loginFailures")
+                    .localCache("work")
+                    .localCache("authorization", (localCache) -> {
+                        localCache.evictionComponent((evict) -> {
+                            evict.strategy(EvictionComponent.Strategy.LRU);
+                            evict.maxEntries(100L);
+
+                        });
+                    })
+                    .localCache("keys", (localCache) -> {
+                        localCache.evictionComponent((evict) -> {
+                            evict.strategy(EvictionComponent.Strategy.LRU);
+                            evict.maxEntries(1000L);
+                        });
+                        localCache.expirationComponent((expire) -> {
+                            expire.maxIdle(3600000L);
+                        });
+                    })
+            );
+        }
+
+    }
+}
