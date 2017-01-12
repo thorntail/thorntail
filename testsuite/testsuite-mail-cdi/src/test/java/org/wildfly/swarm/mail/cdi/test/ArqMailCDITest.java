02bb181417ebd4076994e79a85739855ce959974
Bob McWhirter <bob@mcwhirter.org>
2017-01-12 10:10:21 -0500

    SWARM-840 Mail+CDI fails on Windows (#298)
    Motivation
    ----------
    
    Verifying that Heiko's fix fixes Mail+CDI on Windows.
    
    Modifications
    -------------
    
    Just a test.
    
    Result
    ------
    
    Heiko's fix is verified.

---
+++ testsuite/testsuite-mail-cdi/src/test/java/org/wildfly/swarm/mail/cdi/test/ArqMailCDITest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.mail.cdi.test;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Bob McWhirter
+ */
+@RunWith(Arquillian.class)
+@DefaultDeployment(type = DefaultDeployment.Type.WAR)
+public class ArqMailCDITest {
+
+    @Test
+    public void testIt() throws NamingException {
+        InitialContext context = new InitialContext();
+
+        Object session = context.lookup("java:jboss/mail/default");
+        assertNotNull(session);
+    }
+
+}
