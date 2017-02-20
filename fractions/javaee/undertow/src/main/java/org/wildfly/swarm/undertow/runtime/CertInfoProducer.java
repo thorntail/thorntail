8ac4ac3eb0a058547e6d3d0881d41025c5b46dcb
Aaron Anderson <aaronanderson@acm.org>
2017-02-20 17:19:03 -0600

    SWARM-1103 - Support embedded HTTPS certificate

--- fractions/javaee/undertow/src/main/java/org/wildfly/swarm/undertow/runtime/CertInfoProducer.java
+++ fractions/javaee/undertow/src/main/java/org/wildfly/swarm/undertow/runtime/CertInfoProducer.java
@@ -1,27 +1,35 @@
 package org.wildfly.swarm.undertow.runtime;
 
+import static org.wildfly.swarm.spi.api.Defaultable.bool;
+import static org.wildfly.swarm.spi.api.Defaultable.string;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.jboss.modules.Module;
+import org.jboss.modules.ModuleIdentifier;
 import org.wildfly.swarm.bootstrap.util.TempFileManager;
 import org.wildfly.swarm.spi.api.Defaultable;
 import org.wildfly.swarm.spi.api.annotations.Configurable;
 import org.wildfly.swarm.undertow.UndertowFraction;
 import org.wildfly.swarm.undertow.descriptors.CertInfo;
 
-import static org.wildfly.swarm.spi.api.Defaultable.bool;
-import static org.wildfly.swarm.spi.api.Defaultable.string;
-
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 @ApplicationScoped
 public class CertInfoProducer {
+
+    public static final String JBOSS_DATA_DIR = "jboss.server.data.dir";
 
     @Inject
     UndertowFraction undertow;
@@ -32,28 +40,47 @@ public class CertInfoProducer {
     @Configurable("swarm.https.certificate.generate.host")
     Defaultable<String> selfCertificateHost = string("localhost");
 
+    @Configurable("swarm.https.keystore.embedded")
+    Defaultable<Boolean> embeddedKeystore = bool(false);
+
     @Produces
     @Singleton
     public CertInfo produceCertInfo() {
         if (generateSelfCertificate.get()) {
-            // Remove when SWARM-634 is fixed
-            if (System.getProperty("jboss.server.data.dir") == null) {
-                File tmpDir = null;
-                try {
-                    tmpDir = TempFileManager.INSTANCE.newTempDirectory("wildfly-swarm-data", ".d");
-                    System.setProperty("jboss.server.data.dir", tmpDir.getAbsolutePath());
-                } catch (IOException e) {
-                    // Ignore
-                }
-            }
-            return new CertInfo(selfCertificateHost.get(), "jboss.server.data.dir");
-
+            checkDataDir();
+            return new CertInfo(selfCertificateHost.get(), JBOSS_DATA_DIR);
         } else {
             String keystorePath = undertow.keystorePath();
+            if (embeddedKeystore.get()) {
+                checkDataDir();
+                Path dataDir = Paths.get(System.getProperty(JBOSS_DATA_DIR));
+                Path certDestination = dataDir.resolve(keystorePath);
+                try {
+                    Module appModule = Module.getCallerModuleLoader().loadModule(ModuleIdentifier.create("swarm.application"));
+                    URL jks = appModule.getClassLoader().getResource(keystorePath);
+                    Files.copy(jks.openStream(), certDestination);
+                    keystorePath = certDestination.toString();
+                } catch (Exception ie) {
+                    throw new RuntimeException("Error copying embedded certificate", ie);
+                }
+            }
             String keystorePassword = undertow.keystorePassword();
             String keyPassword = undertow.keyPassword();
             String keystoreAlias = undertow.alias();
             return new CertInfo(keystorePath, keystorePassword, keyPassword, keystoreAlias);
+        }
+    }
+
+    protected void checkDataDir() {
+        // Remove when SWARM-634 is fixed
+        if (System.getProperty(JBOSS_DATA_DIR) == null) {
+            File tmpDir = null;
+            try {
+                tmpDir = TempFileManager.INSTANCE.newTempDirectory("wildfly-swarm-data", ".d");
+                System.setProperty(JBOSS_DATA_DIR, tmpDir.getAbsolutePath());
+            } catch (IOException e) {
+                // Ignore
+            }
         }
     }
 }
