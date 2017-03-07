5bfe6e4f66bd18af2a46d9788a09f9d6a462ebe6
Ken Finnigan <ken@kenfinnigan.me>
2017-03-07 13:13:55 -0500

    SWARM-1166: JUnit Test categories (#408)
    Motivation
    ----------
    We need to be able to exclude tests from community or product builds due to differing functionality.
    
    It's now possible to add `@Category(CommunityOnly.class)` or `@Category(ProductOnly.class)` onto a Test or Test method and have it excluded from the other build type.
    
    Modifications
    -------------
    Added `build-resources` module to contain the interfaces defining the categories, modified the surefire-plugin configuration to set `excludedGroups` based on property that is set by community and product builds.
    
    Result
    ------
    No impact on product, purely for tests.

--- testsuite/testsuite-undertow/src/test/java/org/wildfly/swarm/undertow/HTTPSCustomizerSelfSignedCertificateTest.java
+++ testsuite/testsuite-undertow/src/test/java/org/wildfly/swarm/undertow/HTTPSCustomizerSelfSignedCertificateTest.java
@@ -19,6 +19,7 @@ import java.net.URL;
 
 import javax.net.ssl.SSLHandshakeException;
 
+import category.CommunityOnly;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -26,6 +27,7 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.arquillian.CreateSwarm;
@@ -35,6 +37,7 @@ import org.wildfly.swarm.spi.api.SwarmProperties;
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
+@Category(CommunityOnly.class)
 @RunWith(Arquillian.class)
 public class HTTPSCustomizerSelfSignedCertificateTest {
 
