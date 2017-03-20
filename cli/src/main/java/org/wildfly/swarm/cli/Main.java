708c7691462b13babfef4931d3f0771fadc89fd9
Bob McWhirter <bob@mcwhirter.org>
2017-03-20 15:58:05 -0400

    SWARM-648 - Provide a handy-dandy cli.jar
    Motivation
    ----------
    
    Since we support the management fraction, which can allow CLI'ing,
    we should provide a way to CLI.
    
    Modifications
    -------------
    
    Create a pseudo fraction (because JBoss-Modules and uberjar) for CLI.
    Create a pseudo "standalone server" to produce a cli-swarm.jar.
    
    Result
    ------
    
    cli-swarm.jar.
    
        eduardo:cli bob$ java -jar ./target/cli-2017.4.0-SNAPSHOT-swarm.jar
        You are disconnected at the moment. Type 'connect' to connect to the server or 'help' for the list of supported commands.
        [disconnected /]

---
+++ cli/src/main/java/org/wildfly/swarm/cli/Main.java
@@ -0,0 +1,35 @@
+package org.wildfly.swarm.cli;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import org.jboss.modules.Module;
+import org.jboss.modules.ModuleIdentifier;
+
+/**
+ * @author Bob McWhirter
+ */
+public class Main {
+    private Main() {
+    }
+
+    public static void main(String... args) throws Exception {
+        Path tmpFile = null;
+        if (System.getProperty("jboss.cli.config") == null) {
+            tmpFile = Files.createTempFile("jboss-cli", ".xml");
+            Files.copy(Main.class.getResourceAsStream("/jboss-cli.xml"),
+                    tmpFile,
+                    StandardCopyOption.REPLACE_EXISTING);
+            System.setProperty("jboss.cli.config", tmpFile.toAbsolutePath().toString());
+        }
+        Module cli = Module.getBootModuleLoader().loadModule(ModuleIdentifier.create("org.jboss.as.cli"));
+        try {
+            cli.run(args);
+        } finally {
+            if (tmpFile != null) {
+                Files.delete(tmpFile);
+            }
+        }
+    }
+}
