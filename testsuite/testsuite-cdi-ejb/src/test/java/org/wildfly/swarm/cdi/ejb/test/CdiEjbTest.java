b650b4d29cc9580af6dc5d69179d592133a1d1c2
Ladislav Thon <lthon@redhat.com>
2017-04-07 18:49:26 +0200

    SWARM-1264: add test for CDI + EJB integration
    Motivation
    ----------
    Increase test coverage.
    
    Modifications
    -------------
    Added test for CDI + EJB integration.
    
    Result
    ------
    No change, just more test coverage.

---
+++ testsuite/testsuite-cdi-ejb/src/test/java/org/wildfly/swarm/cdi/ejb/test/CdiEjbTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2017 Red Hat, Inc, and individual contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.cdi.ejb.test;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+@RunWith(Arquillian.class)
+@DefaultDeployment(type = DefaultDeployment.Type.JAR)
+public class CdiEjbTest {
+    @Inject
+    private GreeterFacade greeter;
+
+    @Test
+    public void cdiEjb() {
+        assertEquals("Said: Hello, Ladicek!", greeter.hello("Ladicek"));
+    }
+}
