36204541b58a5f4eb107c321e9571b7f6209227d
Juan G <juangon@gmail.com>
2017-05-05 22:20:48 +0200

    SWARM-1133 Add tests (#489)
    Motivation
    ----------
    Add tests to verify SWARM-1133 fix is working in future.
    
    Modifications
    -------------
    Following a similar approach as in fraction-metadata, FractionUsageAnalyzerTest class, added a simple JUnit test with an MDB to check if right messaging fraction was actually added
    
    Result
    ------
    After these tests, they would only fail if SWARM-1133 is removed some day in future, keeping that feature safe.
    
    - [X] Have you followed the guidelines in our [Contributing](http://wildfly-swarm.io/community/contributing/) document?
    - [X] Have you created a [JIRA](https://issues.jboss.org/browse/SWARM) and used it in the commit message?
    - [X] Have you checked to ensure there aren't other open [Pull Requests](https://github.com/wildfly-swarm/wildfly-swarm/pulls) for the same issue?
    - [X] Have you built the project locally prior to submission with `mvn clean install`?

---
+++ testsuite/testsuite-messaging/src/test/java/org/wildfly/swarm/messaging/test/MessagingTest.java
@@ -0,0 +1,31 @@
+package org.wildfly.swarm.messaging.test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.junit.Test;
+import org.wildfly.swarm.fractions.FractionUsageAnalyzer;
+import org.wildfly.swarm.spi.api.JARArchive;
+
+public class MessagingTest {
+
+    @Test
+    public void testFractionMatching() throws Exception {
+	    JARArchive archive = ShrinkWrap.create(JARArchive.class);
+	    archive.addClass(MyTopicMDB.class);
+	    FractionUsageAnalyzer analyzer = new FractionUsageAnalyzer();
+
+	    final File out = Files.createTempFile(archive.getName(), ".war").toFile();
+	    archive.as(ZipExporter.class).exportTo(out, true);
+
+	    analyzer.source(out);
+	    assertThat(analyzer.detectNeededFractions()
+	                       .stream()
+	                       .filter(fd -> fd.getArtifactId().equals("messaging"))
+	                       .count()).isEqualTo(1);
+    }
+}
