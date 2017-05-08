e6bd42c868805a842ef1ba18910ffb71cabd18cb
Heiko Braun <ike.braun@googlemail.com>
2017-05-08 14:38:42 +0200

    Retain backwards compatibility and fix tests

--- fractions/zipkin-jaxrs/src/main/java/org/wildfly/swarm/jaxrs/btm/ZipkinFraction.java
+++ fractions/zipkin-jaxrs/src/main/java/org/wildfly/swarm/jaxrs/btm/ZipkinFraction.java
@@ -18,6 +18,7 @@ package org.wildfly.swarm.jaxrs.btm;
 import java.util.UUID;
 
 import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.LoggingReporter;
 import com.github.kristofa.brave.Sampler;
 import org.wildfly.swarm.config.runtime.AttributeDocumentation;
 import org.wildfly.swarm.spi.api.Defaultable;
@@ -38,27 +39,48 @@ import zipkin.reporter.urlconnection.URLConnectionSender;
 })
 public class ZipkinFraction implements Fraction<ZipkinFraction> {
 
+    public ZipkinFraction() {
+    }
+
+    public ZipkinFraction(String name) {
+        setName(name);
+    }
+
     public Brave getBraveInstance() {
 
         Brave.Builder builder = new Brave.Builder(name.get());
 
-        AsyncReporter<Span> asyncReporter = AsyncReporter.builder(URLConnectionSender.create(url.get())).build();
-        builder.reporter(asyncReporter)
-                .traceSampler(Sampler.create(rate.get()));
-
+        if (this.url.isDefault()) {
+            builder.reporter(new LoggingReporter())
+                           .traceSampler(Sampler.create(1.0f));
+        } else {
+            AsyncReporter<Span> asyncReporter = AsyncReporter.builder(URLConnectionSender.create(url.get())).build();
+            builder.reporter(asyncReporter)
+                    .traceSampler(Sampler.create(rate.get()));
+        }
         return builder.build();
     }
 
-    /**
-     * The default zipkin server URL (http://localhost:9411/)
-     */
-    private static final String DEFAULT_URL = "http://localhost:9411/api/v1/spans";
+    public ZipkinFraction setName(String name) {
+        this.name.set(name);
+        return this;
+    }
+
+    public ZipkinFraction setUrl(String url) {
+        this.url.set(url);
+        return this;
+    }
+
+    public ZipkinFraction setRate(Float rate) {
+        this.rate.set(rate);
+        return this;
+    }
 
     @AttributeDocumentation("The service name used in reports")
     private Defaultable<String> name = Defaultable.string(UUID.randomUUID().toString());
 
     @AttributeDocumentation("URL of the Zipkin server")
-    private Defaultable<String> url = Defaultable.string(DEFAULT_URL);
+    private Defaultable<String> url = Defaultable.string("http://localhost:9411/");
 
     @AttributeDocumentation("The reporting rate")
     private Defaultable<Float> rate = Defaultable.floating(1.0f);
