d4bd4918e32244259fa48a2395d78b3211f8be9a
Bob McWhirter <bob@mcwhirter.org>
2017-08-30 15:39:26 -0400

    Update and add licenses.

--- core/bootstrap/src/main/java/org/jboss/modules/maven/ArtifactCoordinates.java
+++ core/bootstrap/src/main/java/org/jboss/modules/maven/ArtifactCoordinates.java
@@ -1,13 +1,11 @@
-/*
- * JBoss, Home of Professional Open Source.
- * Copyright 2015 Red Hat, Inc., and individual contributors
- * as indicated by the @author tags.
+/**
+ * Copyright 2015-2017 Red Hat, Inc, and individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jboss.modules.maven;
 
 import java.io.File;
