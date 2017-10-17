c82d87fbb7e00cb020a543926df843021e97269c
Bob McWhirter <bob@mcwhirter.org>
2017-10-17 16:05:01 -0400

    Improve docs
    - Add 'fractions' concept
    - Add HOWTO for autodetecting and expliciting fractions.

---
+++ docs/howto/autodetect-fractions/src/it/java/org/wildfly/swarm/howto/autodetect/AutodetectIT.java
@@ -0,0 +1,27 @@
+package org.wildfly.swarm.howto.autodetect;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Bob McWhirter
+ * @author Ken Finnigan
+ */
+@RunWith(Arquillian.class)
+public class AutodetectIT {
+
+    @Drone
+    WebDriver browser;
+
+    @Test
+    public void testIt() throws Exception {
+        browser.navigate().to("http://localhost:8080/");
+        assertEquals("Hello, Uberjar!", browser.findElement(By.tagName("body")).getText());
+    }
+}
