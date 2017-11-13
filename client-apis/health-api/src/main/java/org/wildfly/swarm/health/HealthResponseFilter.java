0e4661efc0fed35a983ece4271a2cab15f9f2a39
Ken Finnigan <ken@kenfinnigan.me>
2017-11-13 09:54:37 -0500

    Fix issues with reverting of MP 1.2 removal

---
+++ client-apis/health-api/src/main/java/org/wildfly/swarm/health/HealthResponseFilter.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2015-2017 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.health;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class HealthResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext req, ContainerResponseContext resp) throws IOException {
+
+        if (resp.hasEntity() && (resp.getEntity() instanceof Status)) {
+            Status status = (Status) resp.getEntity();
+            int code = (Status.State.UP == status.getState()) ? 200 : 503;
+            resp.setStatus(code);
+            resp.setEntity(status.toJson());
+            resp.getHeaders().putSingle("Content-Type", MediaType.APPLICATION_JSON);
+        }
+    }
+
+}
