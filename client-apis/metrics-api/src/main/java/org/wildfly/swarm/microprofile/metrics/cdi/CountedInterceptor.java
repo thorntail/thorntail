17cae7ea06bd7ac7568e2c8975cd3a1c02ae987a
Heiko Braun <ike.braun@googlemail.com>
2017-11-28 20:16:41 +0100

    Align with recent master

--- client-apis/metrics-api/src/main/java/org/wildfly/swarm/microprofile/metrics/cdi/CountedInterceptor.java
+++ client-apis/metrics-api/src/main/java/org/wildfly/swarm/microprofile/metrics/cdi/CountedInterceptor.java
@@ -1,6 +1,5 @@
 /*
- *
- *   Copyright 2017 Red Hat, Inc, and individual contributors.
+ * Copyright © 2013 Antonin Stefanutti (antonin.stefanutti@gmail.com)
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -13,7 +12,6 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
- * /
  */
 package org.wildfly.swarm.microprofile.metrics.cdi;
 
