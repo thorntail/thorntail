3960a74e35256b61a6c56cd82447723bca1d532c
Ken Finnigan <ken@kenfinnigan.me>
2018-01-12 10:08:05 -0500

    Modify servlet example test to hit URL and check response.
    Ignore for now until config fixed

--- examples/servlet/src/test/java/org/jboss/unimbus/example/ServletTest.java
+++ examples/servlet/src/test/java/org/jboss/unimbus/example/ServletTest.java
@@ -1,12 +1,30 @@
 package org.jboss.unimbus.example;
 
+import io.restassured.RestAssured;
 import org.jboss.unimbus.UNimbus;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
 
 public class ServletTest {
 
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "localhost:8080";
+    }
+
+    @Ignore
     @Test
     public void test() {
         UNimbus.run(MyAppUNimbusConfig.class);
+
+        when()
+                .get("myservlet")
+        .then()
+                .statusCode(200)
+                .body(containsString("Hello from Servlet!"));
     }
 }
