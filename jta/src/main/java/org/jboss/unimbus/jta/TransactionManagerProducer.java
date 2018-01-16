5ce99bd82e606cc9abb410c6a7f2e87ae8dd7b11
Bob McWhirter <bob@mcwhirter.org>
2018-01-16 13:02:42 -0500

    Add Arjuna transaction services for Weld.

---
+++ jta/src/main/java/org/jboss/unimbus/jta/TransactionManagerProducer.java
@@ -0,0 +1,20 @@
+package org.jboss.unimbus.jta;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import com.arjuna.ats.jta.TransactionManager;
+
+
+/**
+ * Created by bob on 1/16/18.
+ */
+@ApplicationScoped
+public class TransactionManagerProducer {
+
+    @Produces
+    @ApplicationScoped
+    javax.transaction.TransactionManager transactionManager() {
+        return TransactionManager.transactionManager();
+    }
+}
