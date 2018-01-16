514385796ad1fb85047cf7a48ad02e278f5a5779
Bob McWhirter <bob@mcwhirter.org>
2018-01-16 11:06:09 -0500

    Tighten up the conditional extension scanning.

--- core/src/main/java/org/jboss/unimbus/condition/ConditionExtension.java
+++ core/src/main/java/org/jboss/unimbus/condition/ConditionExtension.java
@@ -6,10 +6,11 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.enterprise.inject.spi.WithAnnotations;
 
 public class ConditionExtension implements Extension {
 
-    public void perform(@Observes ProcessAnnotatedType<?> event) {
+    public <T> void ifClassPresent(@WithAnnotations({IfClassPresent.class}) @Observes ProcessAnnotatedType<T> event) {
         boolean allowed = true;
 
         AnnotatedType<?> type = event.getAnnotatedType();
@@ -22,6 +23,31 @@ public class ConditionExtension implements Extension {
                     type.getJavaClass().getClassLoader().loadClass(className);
                 } catch (Throwable t) {
                     allowed = false;
+                    break;
+                }
+            }
+        }
+
+        if ( ! allowed ) {
+            event.veto();
+        }
+    }
+
+    public <T> void ifClassNotPresent(@WithAnnotations({IfClassNotPresent.class}) @Observes ProcessAnnotatedType<T> event) {
+        boolean allowed = true;
+
+        AnnotatedType<?> type = event.getAnnotatedType();
+        if (type.isAnnotationPresent(IfClassNotPresent.class)) {
+            Set<IfClassNotPresent> annos = type.getAnnotations(IfClassNotPresent.class);
+
+            for (IfClassNotPresent each : annos) {
+                String className = each.value();
+                try {
+                    type.getJavaClass().getClassLoader().loadClass(className);
+                    allowed = false;
+                    break;
+                } catch (Throwable t) {
+                    // ignore
                 }
             }
         }
--- core/src/main/java/org/jboss/unimbus/condition/IfClassNotPresent.java
+++ core/src/main/java/org/jboss/unimbus/condition/IfClassNotPresent.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.enterprise.util.Nonbinding;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 public @interface IfClassNotPresent {
-    Class<?> value();
+    @Nonbinding
+    String value() default "";
 }
