fc960d09c9e9e4aefa23d7a0a9a613870a760fe7
Marc Savy <marc@rhymewithgravy.com>
2018-01-29 18:56:02 +0000

    Add basic support for: lists, generic types & inheritance.
    Fixed various bugs.

---
+++ fractions/microprofile/microprofile-openapi/src/test/java/org/wildfly/swarm/microprofile/openapi/runtime/Foo.java
@@ -0,0 +1,8 @@
+package org.wildfly.swarm.microprofile.openapi.runtime;
+
+/**
+ * @author Marc Savy {@literal <marc@rhymewithgravy.com>}
+ */
+public class Foo extends Bar {
+    long some_secret_integer = 123;
+}
