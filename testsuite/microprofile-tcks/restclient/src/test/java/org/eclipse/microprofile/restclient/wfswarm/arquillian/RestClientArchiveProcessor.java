c7934acb1c0d354e1c2d116033406e8d40668a25
Heiko Braun <ike.braun@googlemail.com>
2018-02-06 21:01:23 +0100

    SWARM-1688: Eclipse MP REST Client 1.0 impl (#778)
    * Initial import of MP rest client, ported from private repository
    
    * Remove module reference on fraction
    
    * cleanup dependencies
    
    * Accidentaly committed snapshot version
    
    * Carry over provider tests, but disabled the param conversion sceanrio
    
    * Work around param converter limitation in resteasy

---
+++ testsuite/microprofile-tcks/restclient/src/test/java/org/eclipse/microprofile/restclient/wfswarm/arquillian/RestClientArchiveProcessor.java
@@ -0,0 +1,43 @@
+package org.eclipse.microprofile.restclient.wfswarm.arquillian;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
+import org.jboss.arquillian.test.spi.TestClass;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
+import org.wildfly.swarm.undertow.WARArchive;
+
+public class RestClientArchiveProcessor implements ApplicationArchiveProcessor {
+
+    private static Logger log = Logger.getLogger(RestClientArchiveProcessor.class.getName());
+
+    @Override
+    public void process(Archive<?> appArchive, TestClass testClass) {
+
+        if (!(appArchive instanceof WebArchive)) {
+            return;
+        }
+        log.info("Preparing archive: " + appArchive);
+        WARArchive war = appArchive.as(WARArchive.class);
+
+        PomEquippedResolveStage pom = Maven.resolver().loadPomFromFile("pom.xml");
+
+        // Wiremock classes that need to be present
+        File[] wiremockDeps = pom
+                .resolve("com.github.tomakehurst:wiremock")
+                .withTransitivity()
+                .asFile();
+
+        war.addAsLibraries(wiremockDeps);
+
+
+        // TCK Classes that need to present
+        war.addPackages(true, "org.eclipse.microprofile.rest.client.tck.interfaces");
+
+        log.fine("Augmented war: \n" + war.toString(true));
+    }
+}
