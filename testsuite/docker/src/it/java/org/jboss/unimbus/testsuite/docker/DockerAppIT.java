5129bfe070bc02887a49cdbb42331e8fe9d29214
Bob McWhirter <bob@mcwhirter.org>
2018-02-14 16:49:29 -0500

    Begin a test around building multi-layered Docker images.

---
+++ testsuite/docker/src/it/java/org/jboss/unimbus/testsuite/docker/DockerAppIT.java
@@ -0,0 +1,11 @@
+package org.jboss.unimbus.testsuite.docker;
+
+/**
+ * Created by bob on 2/14/18.
+ */
+public class DockerAppIT {
+
+    public void testIt() throws Exception {
+        System.err.println("testing");
+    }
+}
