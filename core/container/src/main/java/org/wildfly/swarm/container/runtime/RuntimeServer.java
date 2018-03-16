83cbca33373e3029764e867b44b01aae1d7cb285
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2018-03-16 10:26:36 +0100

    SWARM-1792: skipping cache activation for unavailable caches

--- core/container/src/main/java/org/wildfly/swarm/container/runtime/RuntimeServer.java
+++ core/container/src/main/java/org/wildfly/swarm/container/runtime/RuntimeServer.java
@@ -20,12 +20,14 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.StreamSupport;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Any;
@@ -198,7 +200,9 @@ public class RuntimeServer implements Server {
 
         List<ServiceActivator> activators = new ArrayList<>();
 
-        this.serviceActivators.forEach(activators::add);
+        StreamSupport.stream(serviceActivators.spliterator(), false)
+                .filter(Objects::nonNull)
+                .forEach(activators::add);
 
         activators.add(new ContentRepositoryServiceActivator(this.contentRepository));
 
---
+++ fractions/wildfly/infinispan/src/main/java/org/wildfly/swarm/infinispan/InfinispanMessages.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.infinispan;
+
+
+import org.jboss.logging.BasicLogger;
+import org.jboss.logging.Logger;
+import org.jboss.logging.annotations.LogMessage;
+import org.jboss.logging.annotations.Message;
+import org.jboss.logging.annotations.MessageLogger;
+
+/**
+ * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com
+ * <br>
+ * Date: 3/15/18
+ */
+@MessageLogger(projectCode = "WFSISPN", length = 4)
+public interface InfinispanMessages extends BasicLogger {
+
+    InfinispanMessages MESSAGES = Logger.getMessageLogger(InfinispanMessages.class, "org.wildfly.swarm.infinispan");
+
+    @LogMessage(level = Logger.Level.DEBUG)
+    @Message(id = 1, value = "Skipping activating cache configuration service: %s. " +
+            "The corresponding fraction is not present.")
+    void skippingCacheActivation(String key);
+}
