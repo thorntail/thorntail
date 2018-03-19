652e42b72520342d6ebcb2aab69114d16cb1803d
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2018-03-19 17:56:09 +0100

    Remove active contexts from BootstrapContextCoordinator after test

--- jca/src/main/java/org/jboss/unimbus/jca/impl/ironjacamar/IronJacamarBootstrap.java
+++ jca/src/main/java/org/jboss/unimbus/jca/impl/ironjacamar/IronJacamarBootstrap.java
@@ -4,6 +4,10 @@ import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 import org.jboss.jca.core.api.workmanager.WorkManager;
 import org.jboss.jca.core.api.bootstrap.CloneableBootstrapContext;
@@ -19,13 +23,30 @@ public class IronJacamarBootstrap {
 
     void setup(@Observes LifecycleEvent.Bootstrap event) {
         WorkManagerCoordinator.getInstance().setDefaultWorkManager(this.workManager);
-        BootstrapContextCoordinator.getInstance().setDefaultBootstrapContext( this.bootstrapContext );
+        BootstrapContextCoordinator.getInstance().setDefaultBootstrapContext(this.bootstrapContext);
     }
 
     @PreDestroy
     void tearDown() {
         WorkManagerCoordinator.getInstance().setDefaultWorkManager(null);
-        BootstrapContextCoordinator.getInstance().setDefaultBootstrapContext(null);
+        BootstrapContextCoordinator contextCoordinator = BootstrapContextCoordinator.getInstance();
+        contextCoordinator.setDefaultBootstrapContext(null);
+
+        getActiveContextIds(contextCoordinator)
+                .forEach(contextCoordinator::removeBootstrapContext);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<String> getActiveContextIds(BootstrapContextCoordinator contextCoordinator) {
+        try {
+            Field activeContextsField = BootstrapContextCoordinator.class.getDeclaredField("activeBootstrapContexts");
+            activeContextsField.setAccessible(true);
+            Map<String, CloneableBootstrapContext> contexts =
+                    (Map<String, CloneableBootstrapContext>) activeContextsField.get(contextCoordinator);
+            return new ArrayList<>(contexts.keySet());
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new RuntimeException("Unable to get active bootstrap contexts", e);
+        }
     }
 
     @Inject
