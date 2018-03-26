07780c5934997802b5f6c8115bd036c1059b5d6f
Bob McWhirter <bob@mcwhirter.org>
2018-03-26 13:13:36 -0400

    Add bean-validation/hibernator-validator.

---
+++ testsuite/bean-validation/src/test/java/org/jboss/unimbus/bean/validation/BeanValidationTest.java
@@ -0,0 +1,39 @@
+package org.jboss.unimbus.bean.validation;
+
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.jboss.unimbus.test.UNimbusTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by bob on 3/26/18.
+ */
+@RunWith(UNimbusTestRunner.class)
+public class BeanValidationTest {
+
+    @Test
+    public void testDefaultValidator() {
+        assertThat( this.validator ).isNotNull();
+
+        Person good = new Person("bob");
+        Set<ConstraintViolation<Person>> violations = this.validator.validate(good);
+        assertThat( violations ).isEmpty();
+
+        Person bad = new Person(null);
+
+        violations = this.validator.validate(bad);
+        assertThat( violations ).hasSize(1);
+    }
+
+
+    @Inject
+    Validator validator;
+
+}
