d85f49b8b7d7451a72ebdc5e3ef0295b57ee74e4
Martin Kouba <mkouba@redhat.com>
2018-03-29 21:49:17 +0200

    SWARM-1755: MP JWT - fix security annotations mapping.
    Motivation
    ----------
    MPJWTAuthExtensionArchivePreparer currently only handles security
    annotations declared on sub-resource methods, i.e. methods annotated with @Path.
    
    Modifications
    -------------
    Fix MPJWTAuthExtensionArchivePreparer to handle all resource methods.
    
    Result
    ------
    All resource methods are processed.

---
+++ testsuite/microprofile-tcks/jwt-auth/src/test/java/org/eclipse/microprofile/jwt/roles/RolesEndpointClassLevel.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016-2017 Contributors to the Eclipse Foundation
+ *
+ *  See the NOTICE file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.eclipse.microprofile.jwt.roles;
+
+import java.security.Principal;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/rolesClass")
+@RolesAllowed("Echoer")
+@RequestScoped
+public class RolesEndpointClassLevel {
+
+    @GET
+    public String echo(@Context SecurityContext sec, @QueryParam("input") String input) {
+        Principal user = sec.getUserPrincipal();
+        return input + ", user="+user.getName();
+    }
+
+}
