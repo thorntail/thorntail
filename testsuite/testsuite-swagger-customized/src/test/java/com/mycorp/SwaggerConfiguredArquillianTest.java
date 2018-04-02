d17d8a856adac0162cfb8715c38a79fbcd7777b5
Harsha Ramesh <roguexz@users.noreply.github.com>
2018-04-02 06:36:31 -0700

    Code cleanup. Refactor duplicate code with Commons-IO utility. (#849)
    Motivation
    ----------
    While working on a solution for SWARM-1667, I noticed (thanks to the IDE) that there was code duplication within the test cases which were trying to read the contents of the deployed archive.
    
    Modifications
    -------------
    * Reuse utility from Commons.IO library.
    
    Result
    ------
    Verified that the build is going through properly.

--- testsuite/testsuite-swagger-customized/src/test/java/com/mycorp/SwaggerConfiguredArquillianTest.java
+++ testsuite/testsuite-swagger-customized/src/test/java/com/mycorp/SwaggerConfiguredArquillianTest.java
@@ -15,12 +15,11 @@
  */
 package com.mycorp;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
+import java.nio.charset.Charset;
 
+import org.apache.commons.io.IOUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -55,31 +54,9 @@ public class SwaggerConfiguredArquillianTest {
     @RunAsClient
     @Test
     public void testEndpoints() throws Exception {
-        String content = getUrlContents("http://127.0.0.1:8080/mycorp/api/swagger.json");
+        String content = IOUtils.toString(new URL("http://127.0.0.1:8080/mycorp/api/swagger.json"), Charset.forName("UTF-8"));
         assertThat(content).contains("\"tags\":[{\"name\":\"theapp\"}]");
         assertThat(content).contains("\"title\":\"My Custom App\"");
     }
 
-    private static String getUrlContents(String theUrl) {
-        StringBuilder content = new StringBuilder();
-
-        try {
-            URL url = new URL(theUrl);
-            URLConnection urlConnection = url.openConnection();
-            BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream())
-            );
-
-            String line;
-
-            while ((line = bufferedReader.readLine()) != null) {
-                content.append(line + "\n");
-            }
-            bufferedReader.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return content.toString();
-    }
 }
