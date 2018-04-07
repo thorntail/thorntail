5712b6a12ad05f4281fbcb55b0a4b802caa9d131
Gregor Tudan <gregor.tudan@cofinpro.de>
2018-04-07 13:26:13 +0200

    SWARM-1892: Add a testsuite for the MVC fraction
    Motivation
    ----------
    We want a testsuite that checks the basic functions of the MVC fraction
    
    Modifications
    -------------
    * A test was added that installs the fraction and tests that ozark is
    activated and views can be rendered.
    * The MVC-fraction now uses a deployment module to activate ozark
    
    Result
    ------
    The MVC fraction is covered by automatic tests

---
+++ testsuite/testsuite-mvc/src/main/resources/views/hello.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
---
+++ testsuite/testsuite-mvc/src/test/java/org/wildfly/swarm/mvc/test/MvcTest.java
@@ -0,0 +1,47 @@
+package org.wildfly.swarm.mvc.test;
+
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Arquillian.class)
+public class MvcTest {
+
+    @ArquillianResource
+    private URL baseUrl;
+
+    @Deployment(testable = false)
+    public static Archive createDeployment() throws Exception {
+        return ShrinkWrap.create(JAXRSArchive.class, "myapp.war")
+                .addClass(MvcApplication.class)
+                .addClass(MvcController.class)
+                .addAsWebInfResource(new ClassLoaderAsset("views/hello.jsp"), "views/hello.jsp")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    @RunAsClient
+    public void testGettingView() throws IOException {
+        Response response = Request.Get(baseUrl.toString() + "/hello").execute();
+        Content content = response.returnContent();
+        assertThat(content.asString(), containsString("Hello World!"));
+    }
+}
