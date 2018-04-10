0a5b4fe0b96970095c1d46d621807d7c122ae7f2
Martin Kouba <mkouba@redhat.com>
2018-04-10 09:26:15 +0200

    SWARM-1944: MP FT - support @Asynchronous @Retry. (#870)
    Motivation
    ----------
    There is a bug in MP FT implementation.
    
    Modifications
    -------------
    Fixed the relevant parts of the code.
    
    Result
    ------
    @Retry with @Asynchronous (and with fallback) works  if an exception is
    thrown from within a business method.

---
+++ fractions/microprofile/microprofile-fault-tolerance/src/test/java/org/wildfly/swarm/microprofile/faulttolerance/deployment/retryonerror/HelloService.java
@@ -0,0 +1,22 @@
+package org.wildfly.swarm.microprofile.faulttolerance.deployment.retry;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.faulttolerance.Retry;
+
+@ApplicationScoped
+public class HelloService {
+
+    static final AtomicInteger COUNTER = new AtomicInteger(0);
+
+    @Retry(maxRetries = 2, retryOn = Error.class)
+    public String retry() {
+        if (COUNTER.incrementAndGet() == 3) {
+            return "ok";
+        }
+        throw new AssertionError();
+    }
+
+}
