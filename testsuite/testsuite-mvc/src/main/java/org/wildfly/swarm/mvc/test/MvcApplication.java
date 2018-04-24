4247618132adeee2a65b8e753798e54f45f76cc3
Gregor Tudan <Gregor@tudan.de>
2018-04-24 13:59:31 +0200

    SWARM-1892: Add a testsuite for the MVC fraction (#884)
    Motivation
    ----------
    We want a testsuite that checks the basic functions of the MVC fraction
    
    Modifications
    -------------
    * A test was added that installs the fraction and tests that ozark is
    activated and views can be rendered.
    * The MVC-fraction now uses a deployment module to activate ozark
    
    Result
    ------
    The MVC fraction is covered by automatic tests

---
+++ testsuite/testsuite-mvc/src/main/java/org/wildfly/swarm/mvc/test/MvcApplication.java
@@ -0,0 +1,9 @@
+package org.wildfly.swarm.mvc.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MvcApplication extends Application {
+
+}
