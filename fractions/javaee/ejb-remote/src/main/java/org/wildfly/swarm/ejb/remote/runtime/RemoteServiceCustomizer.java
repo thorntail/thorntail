8d0d9fa480d1ad4cdd721f84428df633cc6e7b61
Heiko Braun <ike.braun@googlemail.com>
2018-04-25 12:53:13 +0200

    SWARM-1992: Enable ejb-remote security

--- fractions/javaee/ejb-remote/src/main/java/org/wildfly/swarm/ejb/remote/runtime/RemoteServiceCustomizer.java
+++ fractions/javaee/ejb-remote/src/main/java/org/wildfly/swarm/ejb/remote/runtime/RemoteServiceCustomizer.java
@@ -15,13 +15,20 @@
  */
 package org.wildfly.swarm.ejb.remote.runtime;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 import org.wildfly.swarm.config.ejb3.ChannelCreationOptions;
+import org.wildfly.swarm.config.security.Flag;
+import org.wildfly.swarm.config.security.SecurityDomain;
+import org.wildfly.swarm.config.security.security_domain.ClassicAuthentication;
+import org.wildfly.swarm.config.security.security_domain.authentication.LoginModule;
 import org.wildfly.swarm.ejb.EJBFraction;
+import org.wildfly.swarm.security.SecurityFraction;
 import org.wildfly.swarm.spi.api.Customizer;
 import org.wildfly.swarm.spi.api.SwarmProperties;
 import org.wildfly.swarm.spi.runtime.annotations.Post;
@@ -35,6 +42,10 @@ public class RemoteServiceCustomizer implements Customizer {
     @Inject
     @Any
     Instance<EJBFraction> ejbInstance;
+
+    @Inject
+    @Any
+    Instance<SecurityFraction> secInstance;
 
     @Override
     public void customize() {
@@ -51,6 +62,21 @@ public class RemoteServiceCustomizer implements Customizer {
                     opt.type(ChannelCreationOptions.Type.REMOTING);
                 });
             });
+
+            SecurityDomain domain = secInstance.get().subresources().securityDomain("other");
+
+            if (domain != null) {
+                ClassicAuthentication auth = domain.subresources().classicAuthentication();
+                List<LoginModule> loginModules = auth.subresources().loginModules();
+                if (loginModules.stream().noneMatch(lm -> "Remoting".equals(lm.code()))) {
+                    loginModules.add(0, new LoginModule("Remoting")
+                            .code("Remoting")
+                            .flag(Flag.OPTIONAL)
+                            .moduleOption("password-stacking", "useFirstPass"));
+                }
+            } else {
+                throw new IllegalStateException("Expected security domain 'other' to be present!");
+            }
         }
     }
 }
