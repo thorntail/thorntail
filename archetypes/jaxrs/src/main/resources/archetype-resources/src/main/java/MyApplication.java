cbd960af852d85fb3081ec7872debb21954287d1
Bob McWhirter <bob@mcwhirter.org>
2018-05-02 12:03:52 -0400

    Add archetype for jaxrs.
        mvn archetype:generate \
          -DarchetypeGroupId=io.thorntail.archetypes \
          -DarchetypeArtifactId=thorntail-jaxrs-archetype \
          -DarchetypeVersion=4.0.0-SNAPSHOT \
          -DgroupId=com.mycorp \
          -DartifactId=my-app \
          -Dversion=1.0-SNAPSHOT
    
    Closes #6.

---
+++ archetypes/jaxrs/src/main/resources/archetype-resources/src/main/java/MyApplication.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import io.thorntail.Thorntail;
+
+@ApplicationPath("/")
+public class MyApplication extends Application {
+    public static void main(String... args) throws Exception {
+        Thorntail.main(args);
+    }
+}
