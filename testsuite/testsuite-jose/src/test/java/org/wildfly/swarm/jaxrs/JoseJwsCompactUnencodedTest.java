95e94af18085ea8dc3af843e0b65f1b41eccb574
Sergey Beryozkin <sberyozkin@gmail.com>
2018-05-02 11:56:06 +0100

    Adding the detached/unencoded JWS test

---
+++ testsuite/testsuite-jose/src/test/java/org/wildfly/swarm/jaxrs/JoseJwsCompactUnencodedTest.java
@@ -0,0 +1,44 @@
+package org.wildfly.swarm.jaxrs;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.jose.Jose;
+import org.wildfly.swarm.jose.JoseLookup;
+
+@RunWith(Arquillian.class)
+public class JoseJwsCompactUnencodedTest {
+
+    @Deployment
+    public static Archive<?> createDeployment() throws Exception {
+        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+        deployment.addResource(MyResource.class);
+        deployment.addResource(SecuredApplication.class);
+        deployment.addResource(JoseExceptionMapper.class); 
+        deployment.addAllDependencies();
+        deployment.addAsResource("keystore.jks");
+        deployment.addAsResource("project-jws-compact-unencoded.yml", "project-defaults.yml");
+        return deployment;
+    }
+    
+    @Test
+    public void testJwsCompactDetached() throws Exception {
+        Jose jose = JoseLookup.lookup().get();
+        Response r = ClientBuilder.newClient().target("http://localhost:8080/signDetached")
+                                .request(MediaType.TEXT_PLAIN)
+                                .header("DetachedData", "Hello")
+                                .post(Entity.entity(jose.sign("Hello"), MediaType.TEXT_PLAIN));
+        String jws = r.readEntity(String.class);
+        Assert.assertEquals("Hello", jose.verifyDetached(jws, r.getHeaderString("DetachedData")));
+    }
+       
+}
