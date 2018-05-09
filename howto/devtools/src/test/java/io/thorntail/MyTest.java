c63f583ef776322f0480b9faa0f55f06d10467bb
Bob McWhirter <bob@mcwhirter.org>
2018-05-09 09:50:24 -0400

    Create new devtools project.

---
+++ howto/devtools/src/test/java/io/thorntail/MyTest.java
@@ -0,0 +1,20 @@
+package io.thorntail;
+
+import io.thorntail.test.ThorntailTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(ThorntailTestRunner.class)
+public class MyTest {
+
+    @Test
+    public void test() {
+        when().get("/").then()
+                .statusCode(200)
+                .body(containsString("Hello World"));
+    }
+}
+
