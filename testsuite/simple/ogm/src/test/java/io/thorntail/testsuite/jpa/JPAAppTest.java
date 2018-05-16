6beea3d58622f6e0c0c6eaf8d36d9267a4ad520f
Ken Finnigan <ken@kenfinnigan.me>
2018-05-16 08:39:57 -0400

    Fix OGM Testsuite to remove neo4j DB so that subsequent runs with "mvn install" don't fail with existing data problems

--- testsuite/simple/ogm/src/test/java/io/thorntail/testsuite/jpa/JPAAppTest.java
+++ testsuite/simple/ogm/src/test/java/io/thorntail/testsuite/jpa/JPAAppTest.java
@@ -1,6 +1,10 @@
 package io.thorntail.testsuite.jpa;
 
+import java.io.File;
+import java.io.IOException;
+
 import io.thorntail.test.ThorntailTestRunner;
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -9,6 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(ThorntailTestRunner.class)
 public class JPAAppTest {
+	@AfterClass
+	public static void cleanup() throws IOException {
+        deleteDirectory(new File("target", "neo4j.db"));
+	}
+
 	@Test
 	public void test() {
 		String responseBody =
@@ -26,4 +35,18 @@ public class JPAAppTest {
 		assertThat(responseBody).contains("<tr><td>4</td><td>Leonard</td></tr>");
 		assertThat(responseBody).contains("<tr><td>7</td><td>Howard</td></tr>");
 	}
+
+    private static boolean deleteDirectory(File dir) {
+        if (dir.isDirectory()) {
+            File[] children = dir.listFiles();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDirectory(children[i]);
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+
+        return dir.delete();
+    }
 }
