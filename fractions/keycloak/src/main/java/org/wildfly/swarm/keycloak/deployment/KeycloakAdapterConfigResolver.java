d8f38f33790780179be693b9be31629e5a6088c1
Sergey Beryozkin <sberyozkin@gmail.com>
2018-05-21 17:42:16 +0100

    [THORN-2038] Avoiding a possible ordering issue as spotted by Ladislav

--- fractions/keycloak/src/main/java/org/wildfly/swarm/keycloak/deployment/KeycloakAdapterConfigResolver.java
+++ fractions/keycloak/src/main/java/org/wildfly/swarm/keycloak/deployment/KeycloakAdapterConfigResolver.java
@@ -16,6 +16,7 @@
 package org.wildfly.swarm.keycloak.deployment;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
@@ -35,8 +36,13 @@ public class KeycloakAdapterConfigResolver implements KeycloakConfigResolver {
     @Override
     public KeycloakDeployment resolve(OIDCHttpFacade.Request request) {
         String path = request.getRelativePath();
-        return pathDeployments.entrySet().stream().filter(e -> path.startsWith(e.getKey())).findFirst()
-            .map(e -> e.getValue()).orElseThrow(throwException(path));
+        Optional<KeycloakDeployment> dep = Optional.ofNullable(pathDeployments.get(path));
+        return dep.orElse(getMatchingPathDeployment(path).orElseThrow(throwException(path)));
+    }
+
+    private Optional<KeycloakDeployment> getMatchingPathDeployment(String path) {
+        return pathDeployments.entrySet().stream().filter(e -> path.startsWith(e.getKey()))
+            .findFirst().map(e -> e.getValue());
     }
 
     private static Supplier<IllegalStateException> throwException(String path) {
