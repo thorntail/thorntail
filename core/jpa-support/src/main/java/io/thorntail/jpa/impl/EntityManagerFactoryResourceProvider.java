a1328f9557bc14c8e6ed1b1cbb139532eff755ac
Sanne Grinovero <sanne@hibernate.org>
2018-05-28 20:49:31 +0100

    Fixes #944 - Have Hibernate ORM use Narayana Standalone as default JTA Platform

--- core/jpa-support/src/main/java/io/thorntail/jpa/impl/EntityManagerFactoryResourceProvider.java
+++ core/jpa-support/src/main/java/io/thorntail/jpa/impl/EntityManagerFactoryResourceProvider.java
@@ -1,11 +1,15 @@
 package io.thorntail.jpa.impl;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Environment;
 import org.jboss.weld.injection.spi.ResourceReference;
 import org.jboss.weld.injection.spi.ResourceReferenceFactory;
 
@@ -18,7 +22,7 @@ public class EntityManagerFactoryResourceProvider implements ResourceReferenceFa
     
     private EntityManagerFactory entityManagerFactory;
 
-    private final Map<String, String> properties;
+    private final Map<String, Object> properties;
 
     EntityManagerFactoryResourceProvider(String unitName) {
         this(unitName, Collections.emptyMap());
@@ -26,7 +30,7 @@ public class EntityManagerFactoryResourceProvider implements ResourceReferenceFa
 
     EntityManagerFactoryResourceProvider(String unitName, Map<String, String> properties) {
         this.unitName = unitName;
-        this.properties = properties;
+        this.properties = applyDefaultConfigurations(properties);
     }
 
     @Override
@@ -47,4 +51,15 @@ public class EntityManagerFactoryResourceProvider implements ResourceReferenceFa
     public void release() {
         //EntityManagerFactory must be a singleton for compliance with JSR-338
     }
+
+    private static Map<String,Object> applyDefaultConfigurations(Map<String,String> original) {
+        Map<String,Object> properties = new HashMap<>( original );
+        applyDefault(properties, AvailableSettings.JTA_PLATFORM, NarayanaStandaloneJtaPlatform::new);
+        return properties;
+    }
+
+    private static void applyDefault(Map<String,Object> properties, String key, Supplier<Object> valueSource) {
+        properties.computeIfAbsent( key, (k) -> valueSource.get() );
+    }
+
 }
