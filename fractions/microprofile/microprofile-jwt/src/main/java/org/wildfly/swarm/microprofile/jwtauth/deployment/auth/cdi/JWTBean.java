321f2b801701e4cce38bf314c4bb10698e770458
Scott Stark <starksm64@gmail.com>
2018-06-18 00:06:28 -0700

    Testing override of built in Principal bean

---
+++ fractions/microprofile/microprofile-jwt/src/main/java/org/wildfly/swarm/microprofile/jwtauth/deployment/auth/cdi/JWTBean.java
@@ -0,0 +1,97 @@
+package org.wildfly.swarm.microprofile.jwtauth.deployment.auth.cdi;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.PassivationCapable;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.interceptor.Interceptor;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+
+@Priority(Interceptor.Priority.APPLICATION + 10)
+public class JWTBean implements Bean<JsonWebToken>, PassivationCapable {
+    private Set<Type> types;
+    private Set<Annotation> qualifiers;
+    private Class<? extends Annotation> scope = RequestScoped.class;
+
+    JWTBean() {
+        types = new HashSet<>();
+        types.add(JsonWebToken.class);
+        //types.add(Principal.class);
+        types.add(Object.class);
+        qualifiers = new HashSet<>();
+        qualifiers.add(new AnnotationLiteral<Any>() { });
+        qualifiers.add(new AnnotationLiteral<Default>() { });
+    }
+    @Override
+    public Class<?> getBeanClass() {
+        return JsonWebToken.class;
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public boolean isNullable() {
+        return true;
+    }
+
+    @Override
+    public JsonWebToken create(CreationalContext<JsonWebToken> creationalContext) {
+        return MPJWTProducer.getJWTPrincpal();
+    }
+
+    @Override
+    public void destroy(JsonWebToken instance, CreationalContext<JsonWebToken> creationalContext) {
+
+    }
+
+    @Override
+    public Set<Type> getTypes() {
+        return types;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return qualifiers;
+    }
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return scope;
+    }
+
+    @Override
+    public String getName() {
+        return "JsonWebToken";
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public boolean isAlternative() {
+        return false;
+    }
+
+    @Override
+    public String getId() {
+        return "JsonWebToken";
+    }
+}
