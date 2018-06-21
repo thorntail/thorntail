df451a9d760114651919c25265341093e2416418
Sergey Beryozkin <sberyozkin@gmail.com>
2018-06-21 14:01:04 +0100

    Adding the missing resource

---
+++ testsuite/simple/keycloak/src/main/java/io/thorntail/testsuite/security/keycloak/MyJaxrsApplication2.java
@@ -0,0 +1,11 @@
+package io.thorntail.testsuite.security.keycloak;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Ken Finnigan
+ */
+@ApplicationPath("/unsecured")
+public class MyJaxrsApplication2 extends Application {
+}
