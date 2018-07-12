f8d9c13bd48bdec6b69cea2c91cd6c0d32618214
Antoine Sabot-Durand <antoine@sabot-durand.net>
2018-07-12 15:02:29 +0200

    THORN-2063: Migrate MP Fault Tolerance implementation to SmallRye 4.x

--- core/faulttolerance/src/main/java/io/thorntail/faulttolerance/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Support for MicroProfile-Fault Tolerance API.
- *
- * @see <a href="https://github.com/eclipse/microprofile-fault-tolerance">MicroProfile Fault Tolerance</a>
- */
-package io.thorntail.faulttolerance;
