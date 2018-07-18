9bced2adcff9c698936194be7168d331ae5f369d
Pavol Loffay <ploffay@redhat.com>
2018-07-18 15:19:36 +0200

    Fix mockTracerresolver
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- testsuite/tck/opentracing/src/main/java/io/thorntail/opentracing/tck/MockTracerResolver.java
+++ testsuite/tck/opentracing/src/main/java/io/thorntail/opentracing/tck/MockTracerResolver.java
@@ -3,7 +3,6 @@ package io.thorntail.opentracing.tck;
 import io.opentracing.Tracer;
 import io.opentracing.contrib.tracerresolver.TracerResolver;
 import io.opentracing.mock.MockTracer;
-import io.opentracing.util.ThreadLocalActiveSpanSource;
 
 /**
  * Created by bob on 2/19/18.
@@ -12,6 +11,6 @@ public class MockTracerResolver extends TracerResolver {
 
     @Override
     protected Tracer resolve() {
-        return new MockTracer(new ThreadLocalActiveSpanSource(), MockTracer.Propagator.TEXT_MAP);
+        return new MockTracer();
     }
 }
