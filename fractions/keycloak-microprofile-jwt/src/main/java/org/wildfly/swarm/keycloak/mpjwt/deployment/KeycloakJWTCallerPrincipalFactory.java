f44dd6ce200c1f295cae79f5cb12f3a61bc692f1
Sergey Beryozkin <sberyozkin@gmail.com>
2019-03-06 17:52:58 +0100

    Optimizing the factory to create the deployment only once

--- fractions/keycloak-microprofile-jwt/src/main/java/org/wildfly/swarm/keycloak/mpjwt/deployment/KeycloakJWTCallerPrincipalFactory.java
+++ fractions/keycloak-microprofile-jwt/src/main/java/org/wildfly/swarm/keycloak/mpjwt/deployment/KeycloakJWTCallerPrincipalFactory.java
@@ -2,6 +2,7 @@ package org.wildfly.swarm.keycloak.mpjwt.deployment;
 
 import java.io.InputStream;
 
+import org.jboss.logging.Logger;
 import org.keycloak.adapters.KeycloakDeployment;
 import org.keycloak.adapters.KeycloakDeploymentBuilder;
 import org.keycloak.adapters.rotation.AdapterTokenVerifier;
@@ -16,17 +17,30 @@ import io.smallrye.jwt.auth.principal.ParseException;
  * An implementation of the abstract JWTCallerPrincipalFactory that uses the Keycloak token parsing classes.
  */
 public class KeycloakJWTCallerPrincipalFactory extends JWTCallerPrincipalFactory {
+    private static final Logger log = Logger.getLogger(KeycloakJWTCallerPrincipalFactory.class);
+    private static final KeycloakDeployment deployment;
+    static {
+        deployment = createDeployment();
+    }
 
     @Override
     public JWTCallerPrincipal parse(final String token, final JWTAuthContextInfo authContextInfo) throws ParseException {
         try {
-            InputStream keycloakJson = Thread.currentThread().getContextClassLoader().getResourceAsStream("keycloak.json");
-            KeycloakDeployment deployment = KeycloakDeploymentBuilder.build(keycloakJson);
             JWSInput jwsInput = new JWSInput(token);
             AccessToken accessToken = AdapterTokenVerifier.verifyToken(jwsInput.getWireString(), deployment);
             return new KeycloakJWTCallerPrincipal(jwsInput.readContentAsString(), accessToken);
         } catch (Throwable ex) {
             throw new ParseException("Failure to parse the token", ex);
+        }
+    }
+
+    private static KeycloakDeployment createDeployment() {
+        InputStream keycloakJson = Thread.currentThread().getContextClassLoader().getResourceAsStream("keycloak.json");
+        if (keycloakJson == null) {
+            log.warn("keycloak.json resource is not available");
+            return null;
+        } else {
+            return KeycloakDeploymentBuilder.build(keycloakJson);
         }
     }
 }
